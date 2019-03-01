20918e7ec71f23d98ca66ce82a35e08d80abc9d9
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2019-03-01 23:07:31 +0800

    Add files via upload

--- net/net.go
+++ net/net.go
@@ -12,6 +12,9 @@ func socks5_dial(){
 		fmt.Println(err)
 		return
 	}
+
+	//发送socks5验证信息
+	//socks版本 连接方式 验证方式
 	_,err = conn.Write([]byte{5,1,0})
 	var b [1024]byte
 	status,err := conn.Read(b[:])
@@ -23,12 +26,21 @@ func socks5_dial(){
 	fmt.Println(status)
 	fmt.Println("\n")
 
+
+/*
+socks5 protocol
+socks_version link_style none ipv4/ipv6/deamin address port
+socks5协议
+socks版本 连接方式 保留字节 域名/ipv4/ipv6 域名 端口
+*/
 	before := []byte{5,1,0,3,11}
 	de := []byte("youtube.com")
 	port := []byte{byte(8),byte(0)}
 	head_temp := append(before,de...)
 	head := append(head_temp,port...)
+
 	fmt.Println(head)
+
 	_,err = conn.Write(head)
 	if err!=nil{
 		fmt.Println(err)
