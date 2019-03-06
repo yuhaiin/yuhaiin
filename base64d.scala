eafec56f9c901deca0ec294302c331a9e88a8853
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2019-03-06 18:09:23 +0800

    Add files via upload

--- base64d.scala
+++ base64d.scala
@@ -1,22 +1,44 @@
 import java.util.Base64.getUrlDecoder
 //import java.io._
 import scala.io.Source
+import scala.io.StdIn
 
 object base64d {
-  def base64d(str : String):String={
-  val decode = new String(getUrlDecoder.decode((str+"="*(4-str.length)).getBytes()))
-  return decode
-}
+  def base64d(str: String): String = {
+    val decode = new String(getUrlDecoder.decode((str + "=" * (4 - str.length)).getBytes()))
+    decode
+  }
 
-def srr_link_split(str : String):Array[String] = {
-  val lines = Source.fromFile(str).getLines
-  val lines_2 = base64d(lines.next).replaceAll("ssr://","").split("\n")
-  val line_base64d = new Array[String](lines_2.length)
-  for(num <- 0 until line_base64d.length) line_base64d(num) = base64d(lines_2(num))
-  return line_base64d
-}
+  def srr_link_split(str: String): Array[String] = {
+    val lines = Source.fromFile(str).getLines
+    val lines_2 = base64d(lines.next).replaceAll("ssr://", "").split("\n")
+    val line_base64d = new Array[String](lines_2.length)
+    for (num <- line_base64d.indices) line_base64d(num) = base64d(lines_2(num))
+      .replace("/?obfsparam=",":").replace("&protoparam=",":")
+      .replace("&remarks=",":").replace("&group=",":")
+    line_base64d
+  }
+
+  def ssr_list_remarks(line_base64d:Array[String]): Unit ={
+    for(num <- line_base64d.indices){
+      val line_temp = line_base64d(num).split(":")
+      print(num+"."+base64d(line_temp(line_temp.length-2))+"\n")
+    }
+  }
+
+  def menu(): Unit ={
+    print(
+      "1.start ssr\n"+
+      "2.update config\n"+
+      "3.deplay test\n"+
+      ">> "
+    )
+    val num = StdIn.readLine
+    print(num)
+  }
+
   def main(args: Array[String]): Unit = {
-    val lines = srr_link_split("/home/asutorufa/.cache/SSRSub/config.txt")
-    for(line <- lines) print(line+"\n")
+    ssr_list_remarks(srr_link_split("/home/asutorufa/.cache/SSRSub/config.txt"))
+    menu()
   }
 }
