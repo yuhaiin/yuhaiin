525b0cb139fdd15299613926dbc30a09477ee1e8
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2019-04-07 11:15:25 +0800

    Add files via upload

--- process/process.go
+++ process/process.go
@@ -52,10 +52,10 @@ func Start(config_path, db_path string) {
 	cmd.Stderr = &stderr
 	err := cmd.Run()
 	if err != nil {
-		fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
+		fmt.Printf(fmt.Sprint(err) + ": " + stderr.String())
 		return
 	}
-	fmt.Println("Result: " + out.String())
+	fmt.Printf("Result: %s\n", out.String())
 	//fmt.Println(ssr_config.python_path,ssr_config.config_path,ssr_config.log_file,ssr_config.pid_file,ssr_config.fast_open,ssr_config.workers,ssr_config.connect_verbose_info,ssr_config.ssr_path,ssr_config.server,ssr_config.server_port,ssr_config.protocol,ssr_config.method,ssr_config.obfs,ssr_config.password,ssr_config.obfsparam,ssr_config.protoparam)
 }
 
@@ -74,10 +74,10 @@ func Stop(path string) {
 		cmd.Stderr = &stderr
 		err := cmd.Run()
 		if err != nil {
-			fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
+			fmt.Printf(fmt.Sprint(err) + ": " + stderr.String())
 			return
 		}
-		fmt.Println("Result: " + out.String())
+		fmt.Printf("Result: %s", out.String())
 	} else {
 		log.Println("\n")
 		log.Printf("cant find the process: %s", pid)
