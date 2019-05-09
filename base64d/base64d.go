a6ad55f1a03e8cff97457a8a81623168139efa92
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2019-05-09 23:40:02 +0800

    Add files via upload

--- base64d/base64d.go
+++ base64d/base64d.go
@@ -4,11 +4,11 @@ import (
 	"encoding/base64"
 )
 
-//对base64进行长度补全(4的倍数)
+// Base64d 对base64进行长度补全(4的倍数)
 func Base64d(str string) string {
 	for i := 0; i <= len(str)%4; i++ {
 		str += "="
 	}
-	de_str, _ := base64.URLEncoding.DecodeString(str)
-	return string(de_str)
+	deStr, _ := base64.URLEncoding.DecodeString(str)
+	return string(deStr)
 }
