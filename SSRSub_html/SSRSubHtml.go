b02a032864d626706045ce9677a3c71dc467a84f
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2019-05-26 09:55:22 -0500

    Add files via upload

--- SSRSub_html/SSRSubHtml.go
+++ SSRSub_html/SSRSubHtml.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"os"
-	"time"
 
 	getdelay "../net"
 	ssr_process "../process"
 	"../subscription"
+
+	"github.com/apcera/termtables"
 	"github.com/gin-gonic/gin"
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -21,7 +22,7 @@ func main() {
 	// file, _ := exec.LookPath(os.Args[0])
 	// path2, _ := filepath.Abs(file)
 	// rst := filepath.Dir(path2)
-	router.LoadHTMLGlob("/**/**")
+	router.LoadHTMLGlob("./**/**")
 
 	// router.Use(gin.WrapH(http.StripPrefix("/", http.FileServer(box))))
 
@@ -64,9 +65,16 @@ func main() {
 
 	router.GET("/link", func(c *gin.Context) {
 		link := subscription.Get_subscription_link(sqlPath)
+		linkTable := termtables.CreateTable()
+		linkTable.AddTitle("subscription link")
+		for num, i := range link {
+			// linkHTMLElement := `<input class="checkbox" type="checkbox" value="` + i + `" />`
+			linkTable.AddRow(num, i)
+
+		}
 		c.HTML(200, "sidebar.html", gin.H{
 			"subscription": true,
-			"link":         link,
+			"link":         linkTable.Render(),
 		})
 	})
 
@@ -74,19 +82,34 @@ func main() {
 		id := c.Query("id")
 		node := subscription.GetOneNodeAll(id, sqlPath)
 		delay, _ := getdelay.Tcp_delay(node["server"], node["server_port"])
-		c.JSON(200, gin.H{
-			"id":          node["id"],
-			"remarks":     node["remarks"],
-			"server":      node["server"],
-			"server_port": node["server_port"],
-			"protocol":    node["protocol"],
-			"method":      node["method"],
-			"obfs":        node["obfs"],
-			"password":    node["password"],
-			"obfsparam":   node["obfsparam"],
-			"protoparam":  node["protoparam"],
-			"delay":       delay / time.Millisecond,
-		})
+		infTable := termtables.CreateTable()
+		infTable.AddTitle("Node Information")
+		infTable.AddRow("id", node["id"])
+		infTable.AddRow("remarks", node["remarks"])
+		infTable.AddRow("server", node["server"])
+		infTable.AddRow("server_port", node["server_port"])
+		infTable.AddRow("protocol", node["protocol"])
+		infTable.AddRow("method", node["method"])
+		infTable.AddRow("obfs", node["obfs"])
+		infTable.AddRow("password", node["password"])
+		infTable.AddRow("obfsparam", node["obfsparam"])
+		infTable.AddRow("protoparam", node["protoparam"])
+		infTable.AddRow("delay", delay)
+		// fmt.Println("\n" + infTable.Render())
+		c.String(200, infTable.Render())
+		// c.JSON(200, gin.H{
+		// 	"id":          node["id"],
+		// 	"remarks":     node["remarks"],
+		// 	"server":      node["server"],
+		// 	"server_port": node["server_port"],
+		// 	"protocol":    node["protocol"],
+		// 	"method":      node["method"],
+		// 	"obfs":        node["obfs"],
+		// 	"password":    node["password"],
+		// 	"obfsparam":   node["obfsparam"],
+		// 	"protoparam":  node["protoparam"],
+		// 	"delay":       delay / time.Millisecond,
+		// })
 	})
 
 	router.Run(":8081")
