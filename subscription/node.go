65609e7fdf21bef2c6a7d92bd7eb132e95c02cc6
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2019-06-06 21:10:20 +0800

    Add files via upload

--- subscription/node.go
+++ subscription/node.go
@@ -4,15 +4,16 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	"sync"
 	// _ "github.com/mattn/go-sqlite3"
-	//"time"
 )
 
-//打印数据库中的配置文件
-func List_list_db(sql_path string) {
+// GetAllNodeRemarksAndID  like name
+func GetAllNodeRemarksAndID(sqlPath string) [][]string {
 	//访问数据库
-	db := Get_db(sql_path)
+	db, err := sql.Open("sqlite3", sqlPath)
+	if err != nil {
+		fmt.Println(err)
+	}
 	defer db.Close()
 
 	//查找
@@ -20,107 +21,96 @@ func List_list_db(sql_path string) {
 	if err != nil {
 		log.Println(err)
 	}
+	remarksB := [][]string{}
 	//var server,server_port,protocol,method,obfs,password,obfsparam,protoparam string
 	var remarks, id string
 	for rows.Next() {
 		//err = rows.Scan(&server,&server_port,&protocol,&method,&obfs,&password,&obfsparam,&protoparam)
 		err = rows.Scan(&id, &remarks)
-		fmt.Println(id + "." + remarks)
+		// fmt.Println(id + "." + remarks)
+		remarksB = append(remarksB, []string{id, remarks})
 	}
+	// fmt.Println(remarks_)
+	return remarksB
 }
 
-//更换节点(数据库)
-func Ssr_server_node_change(sql_path string) int {
-	List_list_db(sql_path)
-	db := Get_db(sql_path)
+// SsrSQLChangeNode change now node
+func SsrSQLChangeNode(id, sqlPath string) {
+	db := Get_db(sqlPath)
 	defer db.Close()
-
-	//判断数据库是否为空
-	var err error
-	if err = db.QueryRow("SELECT remarks FROM SSR_info;").Scan(err); err == sql.ErrNoRows {
-		log.Println("节点列表为空,请先更新订阅")
-		return 0
-	}
-
-	//获取服务器条数
-	var num int
-	query, err := db.Prepare("select count(1) from SSR_info")
-	query.QueryRow().Scan(&num)
-	//fmt.Println(num)
-
-	fmt.Print("\n输入0返回菜单,输入列表前的数字更换节点>>>")
-	var select_temp int
-	fmt.Scanln(&select_temp)
-	// if select_temp == 0 {
-	switch {
-	case select_temp == 0:
-		return 0
-	case select_temp > 0 && select_temp <= num:
-		/*旧版更新 个人感觉太罗嗦
-		        rows, err := db.Query("SELECT remarks,server,server_port,protocol,method,obfs,password,obfsparam,protoparam FROM SSR_info WHERE id = ?",select_temp)
-		        if err!=nil{
-		            fmt.Println(err)
-		        }
-		        var remarks,server,server_port,protocol,method,obfs,password,obfsparam,protoparam string
-			    for rows.Next(){rows.Scan(&remarks,&server,&server_port,&protocol,&method,&obfs,&password,&obfsparam,&protoparam)}
-
-		        fmt.Println(remarks,server,server_port,protocol,method,obfs,password,obfsparam,protoparam)
-		        //更新表
-		        db.Exec("UPDATE SSR_present_node SET remarks = ?,server = ?,server_port = ?,protocol = ?,method = ?,obfs = ?,password = ?,obfsparam = ?,protoparam = ?",remarks,server,server_port,protocol,method,obfs,password,obfsparam,protoparam)
-		*/
-		//temp := time.Now()
-		db.Exec("BEGIN TRANSACTION;")
-		db.Exec("DELETE FROM SSR_present_node")
-		db.Exec("INSERT INTO SSR_present_node(remarks,server,server_port,protocol,method,obfs,password,obfsparam,protoparam) SELECT remarks,server,server_port,protocol,method,obfs,password,obfsparam,protoparam FROM SSR_info WHERE id = ?", select_temp)
-		db.Exec("COMMIT;")
-	default:
-		fmt.Println("enter error,please retry.")
-		Ssr_server_node_change(sql_path)
-		return 0
-	}
-	return select_temp
-
-}
-
-func Ssr_server_node_init(sql_path string, wg *sync.WaitGroup) {
-
-	db := Get_db(sql_path)
-	//关闭数据库
-	defer db.Close()
-
-	//创建表
 	db.Exec("BEGIN TRANSACTION;")
-	sql_table := `CREATE TABLE IF NOT EXISTS SSR_present_node(
-        remarks TEXT,
-        server TEXT,
-        server_port TEXT,
-        protocol TEXT,
-        method TEXT,
-        obfs TEXT,
-        password TEXT,
-        obfsparam TEXT,
-		protoparam TEXT);`
-	db.Exec(sql_table)
-	//初始化插入空字符
-	//db.Exec("INSERT INTO SSR_present_node(remarks,server,server_port,protocol,method,obfs,password,obfsparam,protoparam)values('none','none','none','none','none','none','none','none','none')")
+	db.Exec("DELETE FROM SSR_present_node")
+	db.Exec("INSERT INTO SSR_present_node(remarks,server,server_port,protocol,"+
+		"method,obfs,password,obfsparam,protoparam) SELECT remarks,server,server_port,"+
+		"protocol,method,obfs,password,obfsparam,protoparam FROM SSR_info WHERE id = ?", id)
 	db.Exec("COMMIT;")
-
-	wg.Done()
 }
 
-func Get_now_node(sql_path string) {
-	db := Get_db(sql_path)
+// GetNowNode like name
+func GetNowNode(sqlPath string) string {
+	db := Get_db(sqlPath)
 	defer db.Close()
 	var remarks string
 	if err := db.QueryRow("SELECT remarks FROM SSR_present_node;").Scan(&remarks); err == sql.ErrNoRows {
 		log.Println("节点列表为空,请先更新订阅")
+		return ""
 	}
-	/*
-			rows, err := db.Query("SELECT remarks FROM SSR_present_node;")
-			var remarks string
-			for rows.Next() {
-				rows.Scan(&remarks)
-		    }
-	*/
-	fmt.Println("当前使用节点:", remarks)
+	return remarks
+}
+
+// GetNowNodeAll Get now node all information
+func GetNowNodeAll(sqlPath string) (map[string]string, error) {
+	node := map[string]string{}
+	db, err := sql.Open("sqlite3", sqlPath)
+	if err != nil {
+		log.Println(err)
+	}
+	defer db.Close()
+
+	var Server, ServerPort, Protocol, Method, Obfs, Password, Obfsparam, Protoparam string
+	err = db.QueryRow("SELECT server,server_port,protocol,method,obfs,password,"+
+		"obfsparam,protoparam FROM SSR_present_node").
+		//Scan(node["Server"],node["Server_port"],node["Protocol"],node["Method"],node["Obfs"],node["Password"],node["Obfsparam"],node["Protoparam"])
+		Scan(&Server, &ServerPort, &Protocol, &Method, &Obfs, &Password, &Obfsparam, &Protoparam)
+
+	if err == sql.ErrNoRows {
+		log.Println("请先选择一个节点,目前没有已选择节点")
+		return node, err
+	}
+	node["server"] = Server
+	node["serverPort"] = ServerPort
+	node["protocol"] = Protocol
+	node["method"] = Method
+	node["obfs"] = Obfs
+	node["password"] = Password
+	node["obfsparam"] = Obfsparam
+	node["protoparam"] = Protoparam
+
+	return node, nil
+}
+
+// GetOneNodeAll like name
+func GetOneNodeAll(id, sqlPath string) map[string]string {
+	db, err := sql.Open("sqlite3", sqlPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+	var idB, remarks, server, serverPort, protocol, method, obfs, password, obfsparam, protoparam string
+	db.QueryRow("SELECT id,remarks,server,server_port,protocol,method,obfs,"+
+		"password,obfsparam,protoparam FROM SSR_info where id = ?", id).
+		Scan(&idB, &remarks, &server, &serverPort, &protocol, &method, &obfs,
+			&password, &obfsparam, &protoparam)
+	return map[string]string{
+		"id":          idB,
+		"remarks":     remarks,
+		"server":      server,
+		"server_port": serverPort,
+		"protocol":    protocol,
+		"method":      method,
+		"obfs":        obfs,
+		"password":    password,
+		"obfsparam":   obfsparam,
+		"protoparam":  protoparam,
+	}
 }
