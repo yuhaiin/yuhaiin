91fc56c13f63ba79d7ebbd0b2362c8b1e45b1477
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2019-06-06 21:08:57 +0800

    Add files via upload

--- process/process.go
+++ process/process.go
@@ -1,128 +1,64 @@
 package ssr_process
 
 import (
-	"bytes"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
-	"regexp"
 	"runtime"
 	"strconv"
 	"strings"
 	"syscall"
-	"time"
 
 	"../config"
-	getdelay "../net"
+	"../subscription"
 )
 
-type ssr_start struct {
-	cmd_temp string
-}
+// Start start ssr
+func Start(configPath, sqlPath string) {
 
-func (*ssr_start) get_string(configPath, dbPath string) (string, config.Ssr_config) {
-	ssrConfig := config.Read_config(configPath, dbPath)
+	pid, status := Get(configPath)
+	if status == true {
+		log.Println("already have run at " + pid)
+		return
+	}
+	argument := config.GetConfigArgument()
+	config := config.GetConfig(configPath, sqlPath)
+	node, _ := subscription.GetNowNodeAll(sqlPath)
+	nodeArgument := []string{"server", "serverPort", "protocol", "method", "obfs", "password", "obfsparam", "protoparam"}
+	argumentArgument := []string{"localAddress", "localPort", "logFile", "pidFile", "workers", "acl", "timeout"}
+	argumentSingle := []string{"fastOpen", "connectVerboseInfo"}
 
-	// Generate shadowsocksr start cmd
-	var argument string
-	nodeArgument := []string{"Server", "Server_port", "Protocol", "Method", "Obfs", "Password", "Obfsparam", "Protoparam"}
-	argumentArgument := []string{"Python_path", "Ssr_path", "Local_address", "Local_port", "Log_file", "Pid_file", "Fast_open", "Workers", "Connect_verbose_info", "Acl", "Timeout", "Deamon"}
-	for _, ssrArgument := range argumentArgument {
-		argument += ssrConfig.Argument[ssrArgument]
-		if ssrArgument == "Connect_verbose_info" {
-			for _, nodeArgument := range nodeArgument {
-				argument += ssrConfig.Node[nodeArgument]
-			}
+	cmdArray := []string{}
+	cmdArray = append(cmdArray, config["ssrPath"])
+	for _, nodeA := range nodeArgument {
+		if node[nodeA] != "" {
+			cmdArray = append(cmdArray, argument[nodeA], node[nodeA])
+		}
+	}
+	for _, argumentA := range argumentArgument {
+		if config[argumentA] != "" {
+			cmdArray = append(cmdArray, argument[argumentA], config[argumentA])
 		}
 	}
 
-	return argument, ssrConfig
-	/*
-		return ssr_config.Argument["Python_path"] + ssr_config.Argument["Ssr_path"] + ssr_config.
-				Argument["Local_address"] + ssr_config.Argument["Local_port"] + ssr_config.
-				Argument["Log_file"] + ssr_config.Argument["Pid_file"] + ssr_config.Argument["Fast_open"] + ssr_config.
-				Argument["Workers"] + ssr_config.Argument["Connect_verbose_info"] + ssr_config.
-				Node["Server"] + ssr_config.Node["Server_port"] + ssr_config.Node["Protocol"] + ssr_config.
-				Node["Method"] + ssr_config.Node["Obfs"] + ssr_config.Node["Password"] + ssr_config.
-				Node["Obfsparam"] + ssr_config.Node["Protoparam"] + ssr_config.
-				Argument["Acl"] + ssr_config.Argument["Timeout"] + ssr_config.Argument["Deamon"], ssr_config.
-				Argument["Local_port"], ssr_config.Argument["Pid_file"], []string{ssr_config.Node["Server"], ssr_config.Node["Server_port"]}
-	*/
-}
-
-func (*ssr_start) windows(path, cmd_temp, Local_port, pid_path string) {
-	vbs_deamon := "CreateObject(\"Wscript.Shell\").run \"cmd /c " + cmd_temp + "\",0"
-	vbs_path := path + "\\SSRSub_deamon.vbs"
-	ioutil.WriteFile(vbs_path, []byte(vbs_deamon), 0644)
-	cmd := exec.Command("cmd", "/c", vbs_path)
-	var out bytes.Buffer
-	var stderr bytes.Buffer
-	cmd.Stdout = &out
-	cmd.Stderr = &stderr
-	err := cmd.Run()
-	if err != nil {
-		fmt.Printf(fmt.Sprint(err) + ": " + stderr.String())
-		return
+	for _, argumentS := range argumentSingle {
+		if config[argumentS] != "" {
+			cmdArray = append(cmdArray, argument[argumentS])
+		}
 	}
-	fmt.Printf("Result: %s\n", out.String())
-
-	time.Sleep(time.Duration(500) * time.Millisecond)
-	cmd = exec.Command("cmd", "/c", "netstat -ano | findstr "+strings.Split(Local_port, " ")[1])
-	cmd.Stdout = &out
-	cmd.Run()
-	re, _ := regexp.Compile(" {2,}")
-	pid_ := strings.Split(re.ReplaceAllString(out.String(), " "), " ")
-	pid := pid_[len(pid_)-1]
-	ioutil.WriteFile(strings.Split(pid_path, " ")[1], []byte(pid), 0644)
-}
-
-func (*ssr_start) other_os(cmd_temp string) {
-	/*
-				get_sh_cmd := exec.Command("which", "sh")
-				var out bytes.Buffer
-				get_sh_cmd.Stdout = &out
-				err := get_sh_cmd.Run()
-				if err != nil {
-					log.Fatal(err)
-					log.Fatal("get sh error.")
-					return
-		        }
-		        cmd = exec.Command(out.String(), "-c", cmd_temp)
-	*/
-	cmd := exec.Command("sh", "-c", cmd_temp)
-
-	var out bytes.Buffer
-	var stderr bytes.Buffer
-	cmd.Stdout = &out
-	cmd.Stderr = &stderr
-	err := cmd.Run()
-	if err != nil {
-		fmt.Printf(fmt.Sprint(err) + ": " + stderr.String())
-		return
+	if runtime.GOOS != "windows" {
+		cmdArray = append(cmdArray, "-d", "start")
 	}
-	fmt.Printf("Result: %s\n", out.String())
-}
-
-func Start(config_path, db_path string) {
-	var ssr_start ssr_start
-	// cmd_temp, Local_port, pid_path, server := ssr_start.get_string(config_path, db_path)
-	cmd_temp, ssrConfig := ssr_start.get_string(config_path, db_path)
-	fmt.Println(cmd_temp)
-	if runtime.GOOS == "windows" {
-		ssr_start.windows(config_path, cmd_temp, ssrConfig.Argument["Local_port"], ssrConfig.Argument["Pid_file"])
-	} else {
-		ssr_start.other_os(cmd_temp)
-	}
-	// fmt.Println(server)
-	delay, err := getdelay.Tcp_delay(strings.Split(ssrConfig.Node["Server"], " ")[1], strings.Split(ssrConfig.Node["Server_port"], " ")[1])
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	fmt.Println("当前节点延迟:", delay)
-	// fmt.Println(ssr_config.python_path,ssr_config.config_path,ssr_config.log_file,ssr_config.pid_file,ssr_config.fast_open,ssr_config.workers,ssr_config.connect_verbose_info,ssr_config.ssr_path,ssr_config.server,ssr_config.server_port,ssr_config.protocol,ssr_config.method,ssr_config.obfs,ssr_config.password,ssr_config.obfsparam,ssr_config.protoparam)
+	fmt.Println(cmdArray)
+	cmd := exec.Command(config["pythonPath"], cmdArray...)
+	cmd.Start()
+	cmd.Wait()
+	// cmd.Process.Release()
+	// cmd.Process.Signal(syscall.SIGUSR1)
+	// fmt.Println(cmd.Process.Pid, config["pidFile"])
+	// ioutil.WriteFile(config["pidFile"], []byte(strconv.Itoa(cmd.Process.Pid)), 0644)
 }
 
 // Stop stop ssr process
@@ -149,7 +85,9 @@ func Stop(path string) {
 			// cmd_temp = "kill " + pid
 			// cmd = exec.Command("sh", "-c", cmd_temp)
 			pidI, _ := strconv.Atoi(pid)
+			// syscall.Kill(pidI, syscall.SIGQUIT)
 			syscall.Kill(pidI, syscall.SIGKILL)
+			// syscall.Kill(pidI, syscall.SIGCHLD)
 		}
 
 		fmt.Println("Process pid=" + pid + " killed!")
@@ -190,6 +128,7 @@ func Get(path string) (pid string, isexist bool) {
 	}
 }
 
+// ----------------------------------old get status-------------------------------------------
 /*
 func Get(path string) (pid string, isexist bool) {
 	config_temp := config.Read_config_file(path)
@@ -238,3 +177,111 @@ func Get(path string) (pid string, isexist bool) {
 	}
 }
 */
+
+// ----------------------------------old Start-------------------------------------------------------------
+
+// type ssr_start struct {
+// 	cmd_temp string
+// }
+
+// func (*ssr_start) get_string(configPath, dbPath string) (string, config.Ssr_config) {
+// 	ssrConfig := config.Read_config(configPath, dbPath)
+
+// 	// Generate shadowsocksr start cmd
+// 	var argument string
+// 	nodeArgument := []string{"Server", "Server_port", "Protocol", "Method", "Obfs", "Password", "Obfsparam", "Protoparam"}
+// 	argumentArgument := []string{"Python_path", "Ssr_path", "Local_address", "Local_port", "Log_file", "Pid_file", "Fast_open", "Workers", "Connect_verbose_info", "Acl", "Timeout", "Deamon"}
+// 	for _, ssrArgument := range argumentArgument {
+// 		argument += ssrConfig.Argument[ssrArgument]
+// 		if ssrArgument == "Connect_verbose_info" {
+// 			for _, nodeArgument := range nodeArgument {
+// 				argument += ssrConfig.Node[nodeArgument]
+// 			}
+// 		}
+// 	}
+
+// 	return argument, ssrConfig
+// 	/*
+// 		return ssr_config.Argument["Python_path"] + ssr_config.Argument["Ssr_path"] + ssr_config.
+// 				Argument["Local_address"] + ssr_config.Argument["Local_port"] + ssr_config.
+// 				Argument["Log_file"] + ssr_config.Argument["Pid_file"] + ssr_config.Argument["Fast_open"] + ssr_config.
+// 				Argument["Workers"] + ssr_config.Argument["Connect_verbose_info"] + ssr_config.
+// 				Node["Server"] + ssr_config.Node["Server_port"] + ssr_config.Node["Protocol"] + ssr_config.
+// 				Node["Method"] + ssr_config.Node["Obfs"] + ssr_config.Node["Password"] + ssr_config.
+// 				Node["Obfsparam"] + ssr_config.Node["Protoparam"] + ssr_config.
+// 				Argument["Acl"] + ssr_config.Argument["Timeout"] + ssr_config.Argument["Deamon"], ssr_config.
+// 				Argument["Local_port"], ssr_config.Argument["Pid_file"], []string{ssr_config.Node["Server"], ssr_config.Node["Server_port"]}
+// 	*/
+// }
+// func (*ssr_start) windows(path, cmd_temp, Local_port, pid_path string) {
+// 	vbs_deamon := "CreateObject(\"Wscript.Shell\").run \"cmd /c " + cmd_temp + "\",0"
+// 	vbs_path := path + "\\SSRSub_deamon.vbs"
+// 	ioutil.WriteFile(vbs_path, []byte(vbs_deamon), 0644)
+// 	cmd := exec.Command("cmd", "/c", vbs_path)
+// 	var out bytes.Buffer
+// 	var stderr bytes.Buffer
+// 	cmd.Stdout = &out
+// 	cmd.Stderr = &stderr
+// 	err := cmd.Run()
+// 	if err != nil {
+// 		fmt.Printf(fmt.Sprint(err) + ": " + stderr.String())
+// 		return
+// 	}
+// 	fmt.Printf("Result: %s\n", out.String())
+
+// 	time.Sleep(time.Duration(500) * time.Millisecond)
+// 	cmd = exec.Command("cmd", "/c", "netstat -ano | findstr "+strings.Split(Local_port, " ")[1])
+// 	cmd.Stdout = &out
+// 	cmd.Run()
+// 	re, _ := regexp.Compile(" {2,}")
+// 	pid_ := strings.Split(re.ReplaceAllString(out.String(), " "), " ")
+// 	pid := pid_[len(pid_)-1]
+// 	ioutil.WriteFile(strings.Split(pid_path, " ")[1], []byte(pid), 0644)
+// }
+
+// func (*ssr_start) other_os(cmd_temp string) {
+// 	/*
+// 				get_sh_cmd := exec.Command("which", "sh")
+// 				var out bytes.Buffer
+// 				get_sh_cmd.Stdout = &out
+// 				err := get_sh_cmd.Run()
+// 				if err != nil {
+// 					log.Fatal(err)
+// 					log.Fatal("get sh error.")
+// 					return
+// 		        }
+// 		        cmd = exec.Command(out.String(), "-c", cmd_temp)
+// 	*/
+// 	cmd := exec.Command("sh", "-c", cmd_temp)
+
+// 	var out bytes.Buffer
+// 	var stderr bytes.Buffer
+// 	cmd.Stdout = &out
+// 	cmd.Stderr = &stderr
+// 	err := cmd.Run()
+// 	if err != nil {
+// 		fmt.Printf(fmt.Sprint(err) + ": " + stderr.String())
+// 		return
+// 	}
+// 	fmt.Printf("Result: %s\n", out.String())
+// }
+
+// func Start(config_path, db_path string) {
+// 	var ssr_start ssr_start
+// 	// cmd_temp, Local_port, pid_path, server := ssr_start.get_string(config_path, db_path)
+// 	cmd_temp, ssrConfig := ssr_start.get_string(config_path, db_path)
+// 	fmt.Println(cmd_temp)
+// 	if runtime.GOOS == "windows" {
+// 		ssr_start.windows(config_path, cmd_temp, ssrConfig.Argument["Local_port"], ssrConfig.Argument["Pid_file"])
+// 	} else {
+// 		ssr_start.other_os(cmd_temp)
+// 	}
+// 	// fmt.Println(server)
+// 	delay, err := getdelay.Tcp_delay(strings.Split(ssrConfig.Node["Server"], " ")[1], strings.Split(ssrConfig.Node["Server_port"], " ")[1])
+// 	if err != nil {
+// 		log.Println(err)
+// 		return
+// 	}
+// 	fmt.Println("当前节点延迟:", delay)
+// 	// fmt.Println(ssr_config.python_path,ssr_config.config_path,ssr_config.log_file,ssr_config.pid_file,ssr_config.fast_open,ssr_config.workers,ssr_config.connect_verbose_info,ssr_config.ssr_path,ssr_config.server,ssr_config.server_port,ssr_config.protocol,ssr_config.method,ssr_config.obfs,ssr_config.password,ssr_config.obfsparam,ssr_config.protoparam)
+// }
