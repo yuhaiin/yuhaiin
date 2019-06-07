2feded10d54580cb267fd8e9040075e4937fa73d
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2019-06-07 10:25:12 +0800

    Add files via upload

--- SSRSub.go
+++ SSRSub.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"runtime"
@@ -12,48 +13,50 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func menu_db(path, db_path string) {
+func menu(configPath, sqlPath string) {
 	//初始化
-	ssr_init.Init(path, db_path)
+	ssr_init.Init(configPath, sqlPath)
 	//获取当前配置文件路径和可执行文件路径
-	ssr_init.Menu_init(path)
+	ssr_init.MenuInit(configPath)
 	//获取当前节点
-	subscription.Get_now_node(db_path)
+	fmt.Println("当前使用节点:", subscription.GetNowNode(sqlPath))
 	for {
 		fmt.Print("1.开启ssr\n2.更换节点/查看所有节点\n3.更新所有订阅\n4.添加订阅链接\n5.删除订阅链接\n6.获取延迟\n7.结束ssr后台\n8.结束此程序(ssr后台运行)\n>>>")
 
-		var select_temp string
-		fmt.Scanln(&select_temp)
+		var selectTemp string
+		fmt.Scanln(&selectTemp)
 
-		switch select_temp {
+		switch selectTemp {
 		case "1":
-			ssr_process.Start(path, db_path)
+			// ssr_process.Start(path, db_path)
+			ssr_process.StartByArgument(configPath, sqlPath)
 		case "2":
-			_, exist := ssr_process.Get(path)
-			select_ := subscription.Ssr_server_node_change(db_path)
-			if exist == true && select_ != 0 {
-				ssr_process.Stop(path)
-				ssr_process.Start(path, db_path)
+			_, exist := ssr_process.Get(configPath)
+			selectB := subscription.Ssr_server_node_change(sqlPath)
+			if exist == true && selectB != 0 {
+				ssr_process.Stop(configPath)
+				// ssr_process.Start(path, db_path)
+				ssr_process.StartByArgument(configPath, sqlPath)
 			}
 			// } else {
 			// 	subscription.Ssr_server_node_change(db_path)
 			// }
 		case "3":
-			subscription.Delete_config_db(db_path)
-			subscription.Add_config_db(db_path)
+			subscription.Delete_config_db(sqlPath)
+			subscription.Add_config_db(sqlPath)
 		case "4":
 			fmt.Print("请输入要添加的订阅链接(一条):")
-			var link_temp string
-			fmt.Scanln(&link_temp)
-			subscription.Subscription_link_add(link_temp, db_path)
+			var linkTemp string
+			fmt.Scanln(&linkTemp)
+			subscription.Subscription_link_add(linkTemp, sqlPath)
 		case "5":
-			subscription.Subscription_link_delete(db_path)
+			subscription.Subscription_link_delete(sqlPath)
 		case "6":
 			//delay_test_temp := config.Read_config_file(path)
 			//GetDelay.Get_delay(strings.Split(delay_test_temp["Local_address"], " ")[1], strings.Split(delay_test_temp["Local_port"], " ")[1])
-			getdelay.Get_tcp_delay(db_path)
+			getdelay.Get_tcp_delay(sqlPath)
 		case "7":
-			ssr_process.Stop(path)
+			ssr_process.Stop(configPath)
 		case "8":
 			os.Exit(0)
 		default:
@@ -63,13 +66,20 @@ func menu_db(path, db_path string) {
 }
 
 func main() {
-	var config_path, sql_path string
+	var configPath, sqlPath string
 	if runtime.GOOS == "windows" {
-		config_path = os.Getenv("USERPROFILE") + "\\Documents\\SSRSub"
-		sql_path = config_path + "\\SSR_config.db"
+		configPath = os.Getenv("USERPROFILE") + "\\Documents\\SSRSub"
+		sqlPath = configPath + "\\SSR_config.db"
 	} else {
-		config_path = os.Getenv("HOME") + "/.config/SSRSub"
-		sql_path = config_path + "/SSR_config.db"
+		configPath = os.Getenv("HOME") + "/.config/SSRSub"
+		sqlPath = configPath + "/SSR_config.db"
 	}
-	menu_db(config_path, sql_path)
+
+	deamon := flag.Bool("d", false, "d")
+	flag.Parse()
+	if *deamon == true {
+		ssr_process.Start(configPath, sqlPath)
+	} else {
+		menu(configPath, sqlPath)
+	}
 }
