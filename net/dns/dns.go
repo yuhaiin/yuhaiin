b2be558d4d7d8d56f61d2a8075f677000ecc4e3e
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2019-08-05 20:23:48 +0800

    fix dns error

--- net/dns/dns.go
+++ net/dns/dns.go
@@ -112,11 +112,20 @@ func DNSv4(DNSServer, domain string) (DNS []string, success bool) {
 	if b[3]&1 == 1 {
 		microlog.Debug("format error!", b[:n])
 		return []string{}, false
+	} else if b[3]&1 == 2 {
+		microlog.Debug("dns server error")
+		return []string{}, false
 	} else if b[3]&1 == 3 {
 		microlog.Debug("no such name", b[:n])
 		return []string{}, false
+	} else if b[3]&1 == 4 {
+		microlog.Debug("dns server not support this request", b[:n])
+		return []string{}, false
+	} else if b[3]&1 == 5 {
+		microlog.Debug("dns server Refuse", b[:n])
+		return []string{}, false
 	} else if b[3]&1 != 0 {
-		microlog.Debug("no such name", b[3]&1, b[3], b[:n])
+		microlog.Debug("other error", b[3]&1, b[3], b[:n])
 		return []string{}, false
 	}
 	// log.Println("header", b[0:12], "qr+opcode+aa+tc+rd:", b[2:3], "ra+z+rcode:", b[3], "rcode:", b[3]&1, "....", b[3]&2, b[3]&4, b[3]&8)
