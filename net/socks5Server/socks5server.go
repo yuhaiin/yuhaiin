432926c54e35d455c69f20143f576d57669adb99
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2019-08-22 12:02:50 +0800

    change io.copy to chan

--- net/socks5Server/socks5server.go
+++ net/socks5Server/socks5server.go
@@ -153,7 +153,7 @@ func (socks5Server *ServerSocks5) handleClientRequest(client net.Conn) {
 						if isMatch {
 							socks5Server.toTCP(client, host, net.JoinHostPort(getDns[0], port))
 						} else {
-							socks5Server.toSocks5(client, net.JoinHostPort(host, port), b[:n])
+							socks5Server.toSocks5(client, net.JoinHostPort(getDns[0], port), b[:n])
 						}
 					} else {
 						microlog.Debug(runtime.NumGoroutine(), host, "dns false")
