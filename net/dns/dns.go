9d787947b877ba633539e0e4dc83a91beb1e49df
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2019-09-05 20:13:36 +0800

    change some package name

--- net/dns/dns.go
+++ net/dns/dns.go
@@ -48,22 +48,35 @@ func DNS(DNSServer, domain string) (DNS []string, success bool) {
 	// |          arcount             |
 	// +------------------------------+
 
-	// • ID：这是由生成DNS查询的程序指定的16位的标志符。该标志符也被随后的应答报文所用，申请者利用这个标志将应答和原来的请求对应起来。
+	// • ID：这是由生成DNS查询的程序指定的16位的标志符。
+	// 该标志符也被随后的应答报文所用，
+	//	申请者利用这个标志将应答和原来的请求对应起来。
 
 	// • QR：该字段占1位，用以指明DNS报文是请求（0）还是应答（1）。
-	// • OPCODE：该字段占4位，用于指定查询的类型。值为0表示标准查询，值为1表示逆向查询，值为2表示查询服务器状态，值为3保留，值为4表示通知，值为5表示更新报文，值6～15的留为新增操作用。
-	// • AA：该字段占1位，仅当应答时才设置。值为1，即意味着正应答的域名服务器是所查询域名的管理机构或者说是被授权的域名服务器。
-	// • TC：该字段占1位，代表截断标志。如果报文长度比传输通道所允许的长而被分段，该位被设为1。
-	// • RD：该字段占1位，是可选项，表示要求递归与否。如果为1，即意味 DNS解释器要求DNS服务器使用递归查询。
+	// • OPCODE：该字段占4位，用于指定查询的类型。
+	//	值为0表示标准查询，值为1表示逆向查询，值为2表示查询服务器状态，
+	//	值为3保留，值为4表示通知，值为5表示更新报文，值6～15的留为新增操作用。
 
-	// • RA：该字段占1位，代表正在应答的域名服务器可以执行递归查询，该字段与查询段无关。
+	// • AA：该字段占1位，仅当应答时才设置。
+	//	值为1，即意味着正应答的域名服务器是所查询域名的
+	//	管理机构或者说是被授权的域名服务器。
+
+	// • TC：该字段占1位，代表截断标志。
+	//	如果报文长度比传输通道所允许的长而被分段，该位被设为1。
+
+	// • RD：该字段占1位，是可选项，表示要求递归与否。
+	//	如果为1，即意味 DNS解释器要求DNS服务器使用递归查询。
+
+	// • RA：该字段占1位，代表正在应答的域名服务器可以执行递归查询，
+	//	该字段与查询段无关。
 	// • Z：该字段占3位，保留字段，其值在查询和应答时必须为0。
 	// • RCODE：该字段占4位，该字段仅在DNS应答时才设置。用以指明是否发生了错误。
 	// 允许取值范围及意义如下：
 	// 0：无错误情况，DNS应答表现为无错误。
 	// 1：格式错误，DNS服务器不能解释应答。
 	// 2：严重失败，因为名字服务器上发生了一个错误，DNS服务器不能处理查询。
-	// 3：名字错误，如果DNS应答来自于授权的域名服务器，意味着DNS请求中提到的名字不存在。
+	// 3：名字错误，如果DNS应答来自于授权的域名服务器，
+	//	意味着DNS请求中提到的名字不存在。
 	// 4：没有实现。DNS服务器不支持这种DNS请求报文。
 	// 5：拒绝，由于安全或策略上的设置问题，DNS名字服务器拒绝处理请求。
 	// 6 ～15 ：留为后用。
@@ -82,7 +95,8 @@ func DNS(DNSServer, domain string) (DNS []string, success bool) {
 	// ANCOUNT -> 2byte
 	// NSCOUNT -> 2byte
 	// ARCOUNT -> 2byte
-	header := []byte{0x01, 0x02, 0x01, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+	header := []byte{0x01, 0x02, 0x01, 0x00, 0x00, 0x01, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00}
 
 	var domainSet []byte
 	for _, domain := range strings.Split(domain, ".") {
@@ -94,7 +108,8 @@ func DNS(DNSServer, domain string) (DNS []string, success bool) {
 	// domain []byte(domainSet), 0x00
 	// qType 0x00,0x01
 	// qClass 0x00,0x01
-	domainSetAndQTypeAndQClass := append(domainSet, 0x00, 0x00, 0x01, 0x00, 0x01)
+	domainSetAndQTypeAndQClass := append(domainSet,
+		0x00, 0x00, 0x01, 0x00, 0x01)
 
 	all := append(header, domainSetAndQTypeAndQClass...)
 
@@ -112,7 +127,8 @@ func DNS(DNSServer, domain string) (DNS []string, success bool) {
 	//microlog.Debug(b[:n])
 
 	if b[3]&1 == 1 {
-		header := []byte{0x01, 0x02, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+		header := []byte{0x01, 0x02, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00}
 		all := append(header, domainSetAndQTypeAndQClass...)
 		conn.Close()
 		conn, err = net.Dial("udp", DNSServer)
@@ -201,23 +217,30 @@ func DNS(DNSServer, domain string) (DNS []string, success bool) {
 		}
 		if int16(answer[answerIndex])<<8+int16(answer[answerIndex+1]) == 0x05 {
 			answerIndex += 8
-			answerIndex += 2 + int(answer[answerIndex])<<8 + int(answer[answerIndex+1])
+			answerIndex += 2 + int(answer[answerIndex])<<8 +
+				int(answer[answerIndex+1])
 		} else {
 			answerIndex += 8
-			if int16(answer[answerIndex])<<8+int16(answer[answerIndex+1]) == 4 {
+			if int16(answer[answerIndex])<<8+
+				int16(answer[answerIndex+1]) == 4 {
 				answerIndex += 2
 				dns = append(dns, strconv.Itoa(int(answer[answerIndex]))+"."+
 					strconv.Itoa(int(answer[answerIndex+1]))+"."+
 					strconv.Itoa(int(answer[answerIndex+2]))+"."+
 					strconv.Itoa(int(answer[answerIndex+3])))
 				answerIndex += 4
-			} else if int16(answer[answerIndex])<<8+int16(answer[answerIndex+1]) == 16 {
+			} else if int16(answer[answerIndex])<<8+
+				int16(answer[answerIndex+1]) == 16 {
 				answerIndex += 2
-				hexDNS := hex.EncodeToString(answer[answerIndex : answerIndex+16])
-				microlog.Debug(hexDNS[0:4] + ":" + hexDNS[4:8] + ":" + hexDNS[8:12] + ":" + hexDNS[12:16] +
-					":" + hexDNS[16:20] + ":" + hexDNS[20:24] + ":" + hexDNS[24:28] + ":" + hexDNS[28:32])
-				dns = append(dns, hexDNS[0:4]+":"+hexDNS[4:8]+":"+hexDNS[8:12]+":"+hexDNS[12:16]+
-					":"+hexDNS[16:20]+":"+hexDNS[20:24]+":"+hexDNS[24:28]+":"+hexDNS[28:32])
+				hexDNS := hex.
+					EncodeToString(answer[answerIndex : answerIndex+16])
+				microlog.Debug(hexDNS[0:4] + ":" + hexDNS[4:8] + ":" +
+					hexDNS[8:12] + ":" + hexDNS[12:16] + ":" +
+					hexDNS[16:20] + ":" + hexDNS[20:24] + ":" +
+					hexDNS[24:28] + ":" + hexDNS[28:32])
+				dns = append(dns, hexDNS[0:4]+":"+hexDNS[4:8]+":"+
+					hexDNS[8:12]+":"+hexDNS[12:16]+":"+hexDNS[16:20]+":"+
+					hexDNS[20:24]+":"+hexDNS[24:28]+":"+hexDNS[28:32])
 
 				answerIndex += 16
 			}
@@ -242,7 +265,8 @@ func DNS(DNSServer, domain string) (DNS []string, success bool) {
 }
 
 // Match <--
-func (dnscache *Cache) Match(host, hostTemplate string, cidrmatch func(string) bool) (isMatched bool) {
+func (dnscache *Cache) Match(host, hostTemplate string,
+	cidrmatch func(string) bool) (isMatched bool) {
 	var isMatch bool
 	if _, exist := dnscache.dns.Load(host); exist == false {
 		if hostTemplate != "ip" {
