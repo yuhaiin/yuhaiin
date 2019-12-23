1287e449bb7720d2051162f20510fe8f40f32eaf
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2019-12-23 11:55:54 +0800

    change daemon method

---
+++ subscription/ssrOrss_test.go
@@ -0,0 +1,11 @@
+package subscription
+
+import (
+	"testing"
+)
+
+func TestGetNode(t *testing.T) {
+	ssr := "ssr://MjIyLjIyMi4yMjIuMjIyOjQ0MzphdXRoX2FlczEyOF9tZDU6Y2hhY2hhMjAtaWV0ZjpodHRwX3Bvc3Q6ZEdWemRBby8/b2Jmc3BhcmFtPWRHVnpkQW8mcHJvdG9wYXJhbT1kR1Z6ZEFvJnJlbWFya3M9ZEdWemRBbyZncm91cD1kR1Z6ZEFvCg"
+	s, _ := GetNode(ssr)
+	t.Log(s)
+}
