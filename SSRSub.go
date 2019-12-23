6c9fbe85c79894b65c1357e11b1a9421db2d4547
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2019-12-23 10:44:21 +0800

    change daemon method

--- SSRSub.go
+++ SSRSub.go
@@ -3,69 +3,67 @@
 package main
 
 import (
-	ssrinit "SsrMicroClient/init"
-	"SsrMicroClient/process"
-	"flag"
 	"log"
 	"os"
 
 	"SsrMicroClient/gui"
+	"SsrMicroClient/init"
 	"SsrMicroClient/process/lockfile"
 )
 
 func main() {
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
 	configPath := ssrinit.GetConfigAndSQLPath()
-	daemon := flag.String("d", "", "d")
-	subDaemon := flag.String("sd", "", "sd")
-	flag.Parse()
+	//daemon := flag.String("d", "", "d")
+	//subDaemon := flag.String("sd", "", "sd")
+	//flag.Parse()
 
-	if *daemon != "" {
-		process.Start(configPath)
+	//if *daemon != "" {
+	//	process.Start(configPath)
+	//}
+	//if *subDaemon != "" {
+	//	if *subDaemon == "ssr" {
+	//		process.Start(configPath)
+	//	}
+	//else if *subDaemon == "http" {
+	//	test.StartHTTP(configPath)
+	//} else if *subDaemon == "httpBp" {
+	//	test.StartHTTPBypass(configPath)
+	//} else if *subDaemon == "httpB" {
+	//	test.StartHTTPByArgument()
+	//} else if *subDaemon == "socks5Bp" {
+	//	test.StartSocks5Bypass(configPath)
+	//} else if *subDaemon == "httpBBp" {
+	//	test.StartHTTPByArgumentBypass()
+	//}
+	//} else {
+	ssrMicroClientGUI, err := gui.NewSsrMicroClientGUI(configPath)
+	if err != nil && ssrMicroClientGUI != nil {
+		ssrMicroClientGUI.MessageBox(err.Error())
 	}
-	if *subDaemon != "" {
-		if *subDaemon == "ssr" {
-			process.Start(configPath)
-		}
-		//else if *subDaemon == "http" {
-		//	test.StartHTTP(configPath)
-		//} else if *subDaemon == "httpBp" {
-		//	test.StartHTTPBypass(configPath)
-		//} else if *subDaemon == "httpB" {
-		//	test.StartHTTPByArgument()
-		//} else if *subDaemon == "socks5Bp" {
-		//	test.StartSocks5Bypass(configPath)
-		//} else if *subDaemon == "httpBBp" {
-		//	test.StartHTTPByArgumentBypass()
-		//}
-	} else {
-		ssrMicroClientGUI, err := gui.NewSsrMicroClientGUI(configPath)
-		if err != nil && ssrMicroClientGUI != nil {
-			ssrMicroClientGUI.MessageBox(err.Error())
-		}
 
-		lockFile, err := os.Create(configPath +
-			"/SsrMicroClientRunStatuesLockFile")
-		if err != nil {
-			ssrMicroClientGUI.MessageBox(err.Error())
-			return
-		}
-
-		if err = lockfile.LockFile(lockFile); err != nil {
-			ssrMicroClientGUI.MessageBox("process is exist!\n" + err.Error())
-			return
-		}
-		defer func() {
-			_ = lockFile.Close()
-			_ = os.Remove(configPath + "/SsrMicroClientRunStatuesLockFile")
-		}()
-
-		if ssrMicroClientGUI.App.IsSessionRestored() {
-			ssrMicroClientGUI.MessageBox("restore is from before")
-		}
-
-		ssrMicroClientGUI.BeforeShow()
-		//ssrMicroClientGUI.MainWindow.Show()
-		ssrMicroClientGUI.App.Exec()
+	lockFile, err := os.Create(configPath +
+		"/SsrMicroClientRunStatuesLockFile")
+	if err != nil {
+		ssrMicroClientGUI.MessageBox(err.Error())
+		return
 	}
+
+	if err = lockfile.LockFile(lockFile); err != nil {
+		ssrMicroClientGUI.MessageBox("process is exist!\n" + err.Error())
+		return
+	}
+	defer func() {
+		_ = lockFile.Close()
+		_ = os.Remove(configPath + "/SsrMicroClientRunStatuesLockFile")
+	}()
+
+	if ssrMicroClientGUI.App.IsSessionRestored() {
+		ssrMicroClientGUI.MessageBox("restore is from before")
+	}
+
+	ssrMicroClientGUI.BeforeShow()
+	//ssrMicroClientGUI.MainWindow.Show()
+	ssrMicroClientGUI.App.Exec()
+	//}
 }
