962e80ea41849a58bfae3303e6e6b08305d7011a
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2020-05-10 15:18:46 +0800

    update forward.

--- net/common/forward.go
+++ net/common/forward.go
@@ -8,12 +8,30 @@ import (
 var (
 	DownloadTotal = 0.0
 	UploadTotal   = 0.0
+	// mode = 0 download mode = 1 upload
+	queue = make(chan struct {
+		mode int
+		num  int
+	}, 100)
 )
+
+func init() {
+	go func() {
+		for s := range queue {
+			switch s.mode {
+			case 0:
+				DownloadTotal += float64(s.num) / 1024.0 / 1024.0
+			case 1:
+				UploadTotal += float64(s.num) / 1024.0 / 1024.0
+			}
+		}
+	}()
+}
 
 func Forward(src, dst net.Conn) {
 	CloseSig := CloseSigPool.Get().(chan error)
-	go pipeStatistic2(src, dst, CloseSig)
-	go pipeStatistic(dst, src, CloseSig)
+	go pipeStatistic(src, dst, CloseSig, 1)
+	go pipeStatistic(dst, src, CloseSig, 0)
 	<-CloseSig
 	<-CloseSig
 	CloseSigPool.Put(CloseSig)
@@ -43,7 +61,7 @@ func pipe(src, dst net.Conn, closeSig chan error) {
 	}
 }
 
-func pipeStatistic(src, dst net.Conn, closeSig chan error) {
+func pipeStatistic(src, dst net.Conn, closeSig chan error, mode int) {
 	buf := BuffPool.Get().([]byte)
 	for {
 		n, err := src.Read(buf[0:])
@@ -54,7 +72,12 @@ func pipeStatistic(src, dst net.Conn, closeSig chan error) {
 			_ = dst.SetDeadline(time.Now())
 			return
 		}
-		DownloadTotal += float64(n) / 1024.0 / 1024.0
+		go func() {
+			queue <- struct {
+				mode int
+				num  int
+			}{mode: mode, num: n}
+		}()
 
 		n, err = dst.Write(buf[0:n])
 		if err != nil {
@@ -78,7 +101,10 @@ func pipeStatistic2(src, dst net.Conn, closeSig chan error) {
 			_ = dst.SetDeadline(time.Now())
 			return
 		}
-		UploadTotal += float64(n) / 1024.0 / 1024.0
+		queue <- struct {
+			mode int
+			num  int
+		}{mode: 1, num: n}
 
 		_, err = dst.Write(buf[0:n])
 		if err != nil {
