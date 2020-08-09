8c9727a49499e8712f85189427c48cabe908190b
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2020-08-09 23:50:17 +0800

    add sysproxy control for windows

--- go.mod
+++ go.mod
@@ -3,15 +3,12 @@ module github.com/Asutorufa/yuhaiin
 go 1.13
 
 require (
-	github.com/aead/chacha20 v0.0.0-20180709150244-8b13a72661da // indirect
 	github.com/golang/protobuf v1.4.2
 	github.com/mzz2017/shadowsocksR v0.0.0-20200722151714-4f4abd8a2d94
 	github.com/shadowsocks/go-shadowsocks2 v0.1.0
 	github.com/therecipe/qt v0.0.0-20200126204426-5074eb6d8c41
 	github.com/therecipe/qt/internal/binding/files/docs/5.12.0 v0.0.0-20200604211348-b56356132618 // indirect
 	github.com/therecipe/qt/internal/binding/files/docs/5.13.0 v0.0.0-20200126204426-5074eb6d8c41 // indirect
-	golang.org/x/text v0.3.0 // indirect
 	google.golang.org/grpc v1.30.0
-	google.golang.org/protobuf v1.23.0 // indirect
 	v2ray.com/core v4.19.1+incompatible
 )
--- gui/sysproxy/sysproxy_windows.go
+++ gui/sysproxy/sysproxy_windows.go
@@ -1,5 +1,125 @@
 package sysproxy
 
-// need to use c/c++ build a dll/executable
-func setSysProxy(http, _ string) {}
-func unsetSysProxy()             {}
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"syscall"
+	"unsafe"
+)
+
+func strPtr(s string) uintptr {
+	return uintptr(unsafe.Pointer(syscall.StringBytePtr(s)))
+}
+
+func getExecPath() (string, error) {
+	file, err := exec.LookPath(os.Args[0])
+	if err != nil {
+		return "", err
+	}
+	execPath, err := filepath.Abs(file)
+	if err != nil {
+		return "", err
+	}
+	return execPath, nil
+}
+
+func getSysProxy() (*syscall.LazyDLL, error) {
+	execPath, err := getExecPath()
+	if err != nil {
+		return nil, err
+	}
+	var dll string
+	if runtime.GOARCH == "amd64" {
+		dll = filepath.Dir(execPath) + "\\static\\dll\\x64\\sysproxydll.dll"
+	} else if runtime.GOARCH == "386" {
+		dll = filepath.Dir(execPath) + "\\static\\dll\\x86\\sysproxydll.dll"
+	} else {
+		return nil, errors.New("not support " + runtime.GOARCH)
+	}
+
+	fmt.Println("System Proxy DLL:", dll)
+	return syscall.NewLazyDLL(dll), nil
+}
+
+func SetSysProxy(http, _ string) {
+	urls, err := url.Parse("//" + http)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	sysproxy, err := getSysProxy()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	setSysProxy := sysproxy.NewProc("SetSystemProxy")
+	ret, _, e1 := syscall.Syscall(setSysProxy.Addr(), 3, strPtr(urls.Hostname()), strPtr(urls.Port()), strPtr(""))
+	if ret == 0 {
+		if e1 != 0 {
+			err = errnoErr(e1)
+		} else {
+			err = syscall.EINVAL
+		}
+	}
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	fmt.Printf("%d.%d", byte(ret), uint8(ret>>8))
+}
+
+func UnsetSysProxy() {
+	sysproxy, err := getSysProxy()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	clearSysproxy := sysproxy.NewProc("ClearSystemProxy")
+	ret, _, e1 := syscall.Syscall(clearSysproxy.Addr(), 0, 0, 0, 0)
+	if ret == 0 {
+		if e1 != 0 {
+			err = errnoErr(e1)
+		} else {
+			err = syscall.EINVAL
+		}
+	}
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	fmt.Printf("%d.%d", byte(ret), uint8(ret>>8))
+}
+
+/*
+ * from https://github.com/golang/sys/blob/master/windows/zsyscall_windows.go#L1073
+ */
+// Do the interface allocations only once for common
+// Errno values.
+const (
+	errnoERROR_IO_PENDING = 997
+)
+
+var (
+	errERROR_IO_PENDING error = syscall.Errno(errnoERROR_IO_PENDING)
+)
+
+// errnoErr returns common boxed Errno values, to prevent
+// allocations at runtime.
+func errnoErr(e syscall.Errno) error {
+	switch e {
+	case 0:
+		return nil
+	case errnoERROR_IO_PENDING:
+		return errERROR_IO_PENDING
+	}
+	// TODO: add more here, after collecting data on the common
+	// error values see on Windows. (perhaps when running
+	// all.bat?)
+	return e
+}
