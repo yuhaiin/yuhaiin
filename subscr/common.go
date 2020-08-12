1c680565d25f5e987f31a6160f15e016b6eed75f
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2020-08-13 00:59:37 +0800

    prepare to manually add node.

--- subscr/common.go
+++ subscr/common.go
@@ -3,13 +3,17 @@ package subscr
 const (
 	shadowsocks  float64 = 1
 	shadowsocksr float64 = 2
+
+	remote float64 = 100
+	manual float64 = 101
 )
 
 type NodeMessage struct {
-	NType  float64 `json:"type"`
-	NHash  string  `json:"hash"`
-	NName  string  `json:"name"`
-	NGroup string  `json:"group"`
+	NType   float64 `json:"type"`
+	NHash   string  `json:"hash"`
+	NName   string  `json:"name"`
+	NGroup  string  `json:"group"`
+	NOrigin float64 `json:"n_origin"`
 }
 
 func interface2string(i interface{}) string {
