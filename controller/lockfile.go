228f8fd43f134d679945ea5f32d46cea4e98055f
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2020-08-17 13:45:37 +0800

    fix some bug. gtk title bug -> https://bugreports.qt.io/browse/QTBUG-75526

--- controller/lockfile.go
+++ controller/lockfile.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"path"
 
 	"github.com/Asutorufa/yuhaiin/config"
 )
@@ -17,16 +18,22 @@ var (
 
 func GetProcessLock(str string) error {
 	var err error
+_retry:
 	lockFile, err = os.OpenFile(LockFilePath, os.O_CREATE|os.O_WRONLY, os.ModePerm)
 	if err != nil {
+		if os.IsNotExist(err) {
+			err = os.MkdirAll(path.Dir(LockFilePath), os.ModePerm)
+			if err != nil {
+				return fmt.Errorf("SettingEncodeJson():MkdirAll -> %v", err)
+			}
+			goto _retry
+		}
 		return fmt.Errorf("GetProcessLock() -> OpenFile() -> %v", err)
 	}
 	if err := LockFile(lockFile); err != nil {
 		return fmt.Errorf("GetProcessLock() -> LockFile() -> %v", err)
 	}
-
 	err = ioutil.WriteFile(hostFile, []byte(str), os.ModePerm)
-	// _, err = lockFile.WriteString(str)
 	if err != nil {
 		log.Printf("GetProcessLock() -> WriteString() -> %v", err)
 	}
@@ -36,6 +43,9 @@ func GetProcessLock(str string) error {
 func ReadLockFile() (string, error) {
 	s, err := ioutil.ReadFile(hostFile)
 	if err != nil {
+		if os.IsNotExist(err) {
+			return "", nil
+		}
 		return "", fmt.Errorf("ReadLockFile() -> ReadFile() -> %v", err)
 	}
 	return string(s), nil
