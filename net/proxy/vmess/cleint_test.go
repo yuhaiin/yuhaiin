8a17f73c720bc22526c5bbe261c4d0ad190f9551
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2021-02-23 17:39:17 +0800

    fix some bugs

--- net/proxy/vmess/cleint_test.go
+++ net/proxy/vmess/cleint_test.go
@@ -9,8 +9,10 @@ import (
 )
 
 func TestNewVmess(t *testing.T) {
-	v, err := NewVmess("x.v2ray.com", 20004, "e70xxx12-4xxxf-xxxe-axx7-46a1xxxxxxxxf",
-		"none", 2, "ws", "/", "www.xxx.com")
+	v, err := NewVmess(
+		"x.v2ray.com", 20004,
+		"e70xxx12-4xxxf-xxxe-axx7-46a1xxxxxxxxf", "none", 2,
+		"ws", "/", "www.xxx.com", false, "", "")
 	if err != nil {
 		t.Error(err)
 		return
