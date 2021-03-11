d6726dc00f2f787e5c1728176d87055113b11896
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2021-03-11 09:29:15 +0800

    fix test

--- net/dns/dot_test.go
+++ net/dns/dot_test.go
@@ -4,8 +4,8 @@ import "testing"
 
 func TestDOT(t *testing.T) {
 	d := NewDOT("223.5.5.5:853")
-	// t.Log(d.Search("www.google.com"))
-	// t.Log(d.Search("www.baidu.com"))
+	t.Log(d.Search("www.google.com"))
+	t.Log(d.Search("www.baidu.com"))
 	// d.SetServer("dot.pub:853") //  not support ENDS, so shit
 	// t.Log(d.Search("www.google.com"))
 	// t.Log(d.Search("www.baidu.com"))
