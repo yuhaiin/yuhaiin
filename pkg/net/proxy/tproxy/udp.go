b0db7b2875f802a63cba29a76e72440a07a6cdb1
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2021-07-13 13:37:14 +0800

    remove useless code

--- pkg/net/proxy/tproxy/udp.go
+++ pkg/net/proxy/tproxy/udp.go
@@ -16,44 +16,42 @@ import (
 	"github.com/Asutorufa/yuhaiin/pkg/net/proxy/proxy"
 )
 
-func control() func(network, address string, c syscall.RawConn) error {
-	return func(network, address string, c syscall.RawConn) error {
-		var fn = func(s uintptr) {
-			err := syscall.SetsockoptInt(int(s), syscall.SOL_IP, syscall.IP_TRANSPARENT, 1)
-			if err != nil {
-				log.Printf("set socket with SOL_IP, IP_TRANSPARENT failed: %v", err)
-			}
-
-			val, err := syscall.GetsockoptInt(int(s), syscall.SOL_IP, syscall.IP_TRANSPARENT)
-			if err != nil {
-				log.Printf("get socket with SOL_IP, IP_TRANSPARENT failed: %v", err)
-			} else {
-				log.Printf("value of IP_TRANSPARENT option is: %d", int(val))
-			}
-
-			err = syscall.SetsockoptInt(int(s), syscall.SOL_IP, syscall.IP_RECVORIGDSTADDR, 1)
-			if err != nil {
-				log.Printf("set socket with SOL_IP, IP_RECVORIGDSTADDR failed: %v", err)
-			}
-
-			val, err = syscall.GetsockoptInt(int(s), syscall.SOL_IP, syscall.IP_RECVORIGDSTADDR)
-			if err != nil {
-				log.Printf("get socket with SOL_IP, IP_RECVORIGDSTADDR failed: %v", err)
-			} else {
-				log.Printf("value of IP_RECVORIGDSTADDR option is: %d", int(val))
-			}
-		}
-
-		if err := c.Control(fn); err != nil {
-			return err
-		}
-
-		return nil
-	}
+func NewServer(host, username, password string) (proxy.Server, error) {
+	return proxy.NewUDPServer(host, proxy.WithListenConfig(net.ListenConfig{Control: control}), proxy.WithListenFunc(handleUDP))
 }
 
-func NewServer(host, username, password string) (proxy.Server, error) {
-	return proxy.NewUDPServer(host, proxy.WithListenConfig(net.ListenConfig{Control: control()}), proxy.WithListenFunc(handleUDP))
+func control(network, address string, c syscall.RawConn) error {
+	var fn = func(s uintptr) {
+		err := syscall.SetsockoptInt(int(s), syscall.SOL_IP, syscall.IP_TRANSPARENT, 1)
+		if err != nil {
+			log.Printf("set socket with SOL_IP, IP_TRANSPARENT failed: %v", err)
+		}
+
+		val, err := syscall.GetsockoptInt(int(s), syscall.SOL_IP, syscall.IP_TRANSPARENT)
+		if err != nil {
+			log.Printf("get socket with SOL_IP, IP_TRANSPARENT failed: %v", err)
+		} else {
+			log.Printf("value of IP_TRANSPARENT option is: %d", int(val))
+		}
+
+		err = syscall.SetsockoptInt(int(s), syscall.SOL_IP, syscall.IP_RECVORIGDSTADDR, 1)
+		if err != nil {
+			log.Printf("set socket with SOL_IP, IP_RECVORIGDSTADDR failed: %v", err)
+		}
+
+		val, err = syscall.GetsockoptInt(int(s), syscall.SOL_IP, syscall.IP_RECVORIGDSTADDR)
+		if err != nil {
+			log.Printf("get socket with SOL_IP, IP_RECVORIGDSTADDR failed: %v", err)
+		} else {
+			log.Printf("value of IP_RECVORIGDSTADDR option is: %d", int(val))
+		}
+	}
+
+	if err := c.Control(fn); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func handleUDP(l net.PacketConn, p proxy.Proxy) error {
