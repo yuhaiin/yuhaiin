25a573f5f310e9fd6a20ee9fe7833cdc739375f1
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2021-07-20 20:27:00 +0800

    update main

--- cmd/yuhaiin/main.go
+++ cmd/yuhaiin/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"net/http"
+	_ "net/http/pprof"
 	"os"
 	"os/exec"
 	"os/signal"
@@ -13,15 +15,105 @@ import (
 	"syscall"
 	"time"
 
-	"net/http"
-	_ "net/http/pprof"
-
 	"github.com/Asutorufa/yuhaiin/internal/api"
 	"github.com/Asutorufa/yuhaiin/internal/app"
 	"github.com/Asutorufa/yuhaiin/internal/config"
 	"github.com/Asutorufa/yuhaiin/pkg/subscr"
 	"google.golang.org/grpc"
 )
+
+func init() {
+	log.SetFlags(log.Llongfile)
+
+	go func() {
+		// pprof
+		_ = http.ListenAndServe("0.0.0.0:6060", nil)
+	}()
+
+	go func() {
+		signChannel := make(chan os.Signal, 5)
+		signal.Notify(signChannel, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+		for s := range signChannel {
+			switch s {
+			case syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT:
+				log.Println("stop server")
+				grpcServer.Stop()
+			default:
+				fmt.Println("OTHERS SIGN:", s)
+			}
+		}
+	}()
+}
+
+var grpcServer = grpc.NewServer(grpc.EmptyServerOption{})
+
+// protoc --go_out=plugins=grpc:. --go_opt=paths=source_relative api/api.proto
+func main() {
+	host := flag.String("host", "127.0.0.1:50051", "RPC SERVER HOST")
+	configDir := flag.String("cd", defaultConfigDir(), "config dir")
+	kwdc := flag.Bool("kwdc", false, "kill process when grpc disconnect")
+	flag.Parse()
+
+	fmt.Println("save config at:", *configDir)
+	fmt.Println("gRPC Listen Host:", *host)
+
+	lock, err := app.NewLock(filepath.Join(*configDir, "yuhaiin.lock"), *host)
+	if err != nil {
+		panic(err)
+	}
+	defer lock.UnLock()
+
+	// initialize Local Servers Controller
+	lis, err := net.Listen("tcp", *host)
+	if err != nil {
+		panic(err)
+	}
+
+	if !*kwdc {
+		stopWithParentExited()
+	}
+
+	/*
+	* net.Conn/net.PacketConn
+	*    |
+	* nodeManger
+	*    |
+	* BypassManager
+	*    |
+	* statis/connection manager
+	*    |
+	* listener
+	 */
+
+	nodeManager, err := subscr.NewNodeManager(filepath.Join(*configDir, "node.json"))
+	if err != nil {
+		panic(err)
+	}
+
+	conf, err := config.NewConfig(*configDir)
+	if err != nil {
+		panic(err)
+	}
+
+	flowStatis := app.NewConnManager(app.NewBypassManager(conf, nodeManager))
+
+	_, err = app.NewListener(conf, flowStatis)
+	if err != nil {
+		log.Printf("create new listener failed: %v\n", err)
+	}
+
+	grpcServer.RegisterService(&api.Config_ServiceDesc, api.NewConfig(conf, flowStatis))  // TODO Deprecated
+	grpcServer.RegisterService(&api.Node_ServiceDesc, api.NewNode(nodeManager))           // TODO Deprecated
+	grpcServer.RegisterService(&api.Subscribe_ServiceDesc, api.NewSubscribe(nodeManager)) // TODO Deprecated
+	grpcServer.RegisterService(&api.ProcessInit_ServiceDesc, api.NewProcess(lock, *host))
+	grpcServer.RegisterService(&subscr.NodeManager_ServiceDesc, nodeManager)
+	grpcServer.RegisterService(&config.ConfigDao_ServiceDesc, conf)
+	grpcServer.RegisterService(&app.Connections_ServiceDesc, flowStatis)
+	err = grpcServer.Serve(lis)
+	if err != nil {
+		panic(err)
+	}
+}
 
 func defaultConfigDir() (Path string) {
 	var err error
@@ -47,44 +139,8 @@ func defaultConfigDir() (Path string) {
 	return
 }
 
-func init() {
-	log.SetFlags(log.Llongfile)
-
+func stopWithParentExited() {
 	go func() {
-		// 开启pprof，监听请求
-		err := http.ListenAndServe("0.0.0.0:6060", nil)
-		if err != nil {
-			fmt.Printf("start pprof failed on %s\n", "0.0.0.0:6060")
-		}
-	}()
-
-	go func() {
-		signChannel := make(chan os.Signal, 5)
-		signal.Notify(signChannel, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-		for s := range signChannel {
-			switch s {
-			case syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT:
-				log.Println("kernel exit")
-				os.Exit(0)
-			default:
-				fmt.Println("OTHERS SIGN:", s)
-			}
-		}
-	}()
-}
-
-// protoc --go_out=plugins=grpc:. --go_opt=paths=source_relative api/api.proto
-func main() {
-	host := flag.String("host", "127.0.0.1:50051", "RPC SERVER HOST")
-	configDir := flag.String("cd", defaultConfigDir(), "config dir")
-	kwdc := flag.Bool("kwdc", false, "kill process when grpc disconnect")
-	flag.Parse()
-
-	go func() {
-		if !*kwdc {
-			return
-		}
-
 		ppid := os.Getppid()
 		ticker := time.NewTicker(time.Second)
 
@@ -94,68 +150,7 @@ func main() {
 			}
 
 			log.Println("checked parent already exited, exit myself.")
-			os.Exit(0)
+			grpcServer.Stop()
 		}
 	}()
-
-	fmt.Println("save config at:", *configDir)
-	fmt.Println("gRPC Listen Host:", *host)
-
-	conf, err := config.NewConfig(*configDir)
-	if err != nil {
-		panic(err)
-	}
-
-	/*
-	* net.Conn/net.PacketConn
-	*    |
-	* nodeManger
-	*    |
-	* BypassManager
-	*    |
-	* statis/connection manager
-	*    |
-	* listener
-	 */
-
-	var nodeManager *subscr.NodeManager
-	var flowStatis *app.ConnManager
-	// initialize Local Servers Controller
-	l, err := app.NewListener(conf, nil)
-	if err != nil {
-		log.Printf("create new listener failed: %v\n", err)
-	} else {
-		nodeManager, err = subscr.NewNodeManager(filepath.Join(*configDir, "node.json"))
-		if err != nil {
-			panic(err)
-		}
-		flowStatis = app.NewConnManager(app.NewBypassManager(conf, nodeManager))
-		l.SetProxy(flowStatis)
-	}
-
-	lis, err := net.Listen("tcp", *host)
-	if err != nil {
-		panic(err)
-	}
-
-	lock := app.NewLock(filepath.Join(*configDir, "yuhaiin.lock"))
-	defer lock.UnLock()
-	if err = lock.Lock(*host); err != nil {
-		panic(err)
-	}
-
-	s := grpc.NewServer(grpc.EmptyServerOption{})
-
-	s.RegisterService(&api.Config_ServiceDesc, api.NewConfig(conf, flowStatis))  // TODO Deprecated
-	s.RegisterService(&api.Node_ServiceDesc, api.NewNode(nodeManager))           // TODO Deprecated
-	s.RegisterService(&api.Subscribe_ServiceDesc, api.NewSubscribe(nodeManager)) // TODO Deprecated
-
-	s.RegisterService(&api.ProcessInit_ServiceDesc, api.NewProcess(lock, *host))
-	s.RegisterService(&subscr.NodeManager_ServiceDesc, nodeManager)
-	s.RegisterService(&config.ConfigDao_ServiceDesc, conf)
-	s.RegisterService(&app.Connections_ServiceDesc, flowStatis)
-	err = s.Serve(lis)
-	if err != nil {
-		panic(err)
-	}
 }
