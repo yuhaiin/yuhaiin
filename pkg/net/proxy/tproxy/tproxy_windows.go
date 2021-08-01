cc368456f6b3a0c2488f339123f55293c17ed23b
asutorufa <2429um0f9@relay.firefox.com>
2021-08-02 02:14:49 +0800

    fix windows

---
+++ pkg/net/proxy/tproxy/tproxy_windows.go
@@ -0,0 +1,12 @@
+//+build windows
+package tproxy
+
+import (
+	"fmt"
+
+	"github.com/Asutorufa/yuhaiin/pkg/net/proxy/proxy"
+)
+
+func NewServer(h string) (proxy.Server, error) {
+	return nil, fmt.Errorf("tproxy not support for windows")
+}
