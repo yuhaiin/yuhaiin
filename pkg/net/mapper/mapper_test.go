30105e32469889e7eb09e296c60be70dd38c81b6
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2021-10-13 17:21:38 +0800

    fmt && add dns mock

--- pkg/net/mapper/mapper_test.go
+++ pkg/net/mapper/mapper_test.go
@@ -1,29 +1,42 @@
 package mapper
 
 import (
+	"net"
 	"testing"
 
 	"github.com/Asutorufa/yuhaiin/pkg/net/dns"
+	mock_dns "github.com/Asutorufa/yuhaiin/pkg/net/dns/mock"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewMatcher(t *testing.T) {
-	matcher := NewMapper(dns.NewDoH("223.5.5.5", nil, nil).LookupIP)
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	dns := mock_dns.NewMockDNS(ctrl)
+	dns.EXPECT().LookupIP("www.twitter.com").Return([]net.IP{net.ParseIP("10.2.2.1")}, nil)
+	dns.EXPECT().LookupIP("www.facebook.com").Return([]net.IP{net.ParseIP("10.2.2.1")}, nil)
+	dns.EXPECT().LookupIP("www.google.com").Return([]net.IP{net.ParseIP("127.0.0.1")}, nil)
+
+	matcher := NewMapper(dns.LookupIP)
 	matcher.Insert("*.baidu.com", "test_baidu")
 	matcher.Insert("10.2.2.1/18", "test_cidr")
 	matcher.Insert("*.163.com", "163")
 	matcher.Insert("music.126.com", "126")
 	matcher.Insert("*.advertising.com", "advertising")
-	t.Log(matcher.Search("10.2.2.1"))             // true
-	t.Log(matcher.Search("www.baidu.com"))        // true
-	t.Log(matcher.Search("passport.baidu.com"))   // true
-	t.Log(matcher.Search("tieba.baidu.com"))      // true
-	t.Log(matcher.Search("www.google.com"))       // false
-	t.Log(matcher.Search("test.music.163.com"))   // true
-	t.Log(matcher.Search("guce.advertising.com")) // true
-	t.Log(matcher.Search("www.twitter.com"))      // false
-	t.Log(matcher.Search("www.facebook.com"))     // false
-	t.Log(matcher.Search("127.0.0.1"))            // false
-	t.Log(matcher.Search("ff::"))                 // false
+
+	assert.Equal(t, "test_cidr", matcher.Search("10.2.2.1"))            // true
+	assert.Equal(t, "test_baidu", matcher.Search("www.baidu.com"))      // true
+	assert.Equal(t, "test_baidu", matcher.Search("passport.baidu.com")) // true
+	assert.Equal(t, "test_baidu", matcher.Search("tieba.baidu.com"))    // true
+	assert.Equal(t, nil, matcher.Search("www.google.com"))
+	assert.Equal(t, "163", matcher.Search("test.music.163.com"))
+	assert.Equal(t, "advertising", matcher.Search("guce.advertising.com"))
+	assert.Equal(t, "test_cidr", matcher.Search("www.twitter.com")) // false
+	assert.Equal(t, "test_cidr", matcher.Search("www.facebook.com"))
+	assert.Equal(t, nil, matcher.Search("127.0.0.1"))
+	assert.Equal(t, nil, matcher.Search("ff::"))
 }
 
 func BenchmarkMapper(b *testing.B) {
