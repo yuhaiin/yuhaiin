154aaa0db08676b96e28187dc695aaa649482706
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2021-10-17 19:36:06 +0800

    rewrite proxy

--- pkg/net/proxy/shadowsocks/client.go
+++ pkg/net/proxy/shadowsocks/client.go
@@ -3,7 +3,6 @@ package shadowsocks
 import (
 	"bytes"
 	"fmt"
-	"log"
 	"net"
 	"strconv"
 	"strings"
@@ -11,7 +10,6 @@ import (
 	"github.com/Asutorufa/yuhaiin/pkg/net/proxy/proxy"
 	ss5client "github.com/Asutorufa/yuhaiin/pkg/net/proxy/socks5/client"
 	ss5server "github.com/Asutorufa/yuhaiin/pkg/net/proxy/socks5/server"
-	"github.com/Asutorufa/yuhaiin/pkg/net/utils"
 	"github.com/shadowsocks/go-shadowsocks2/core"
 )
 
@@ -24,71 +22,27 @@ var (
 
 //Shadowsocks shadowsocks
 type Shadowsocks struct {
-	cipher     core.Cipher
-	server     string
-	port       string
-	plugin     string
-	pluginOpt  string
-	pluginFunc func(conn net.Conn) (net.Conn, error)
+	cipher core.Cipher
+	server string
+	port   string
 
-	p proxy.Proxy
+	p       proxy.Proxy
+	udpAddr net.Addr
 }
 
-//NewShadowsocks new shadowsocks client
-func NewShadowsocks(cipherName string, password string, server, port string,
-	plugin, pluginOpt string) (proxy.Proxy, error) {
-	cipher, err := core.PickCipher(strings.ToUpper(cipherName), nil, password)
-	if err != nil {
-		return nil, err
-	}
-	s := &Shadowsocks{
-		cipher:    cipher,
-		server:    server,
-		port:      port,
-		plugin:    strings.ToUpper(plugin),
-		pluginOpt: pluginOpt,
-
-		p: utils.NewClientUtil(server, port),
-	}
-	switch strings.ToLower(plugin) {
-	case OBFS:
-		s.pluginFunc = func(conn net.Conn) (net.Conn, error) {
-			conn, err := NewObfs(conn, pluginOpt)
-			if err != nil {
-				log.Println(err)
-				return nil, fmt.Errorf("create obfs plugin failed: %v", err)
-			}
-			return conn, nil
-		}
-	case V2RAY:
-		s.pluginFunc = func(conn net.Conn) (net.Conn, error) {
-			conn, err := NewV2raySelf(conn, pluginOpt)
-			if err != nil {
-				log.Println(err)
-				return nil, fmt.Errorf("create v2ray plugin failed: %v", err)
-			}
-			return conn, nil
-		}
-	default:
-		s.pluginFunc = func(conn net.Conn) (net.Conn, error) { return conn, nil }
-	}
-
-	return s, nil
-}
-
-func NewShadowsocks2(cipherName string, password string, server, port string) func(proxy.Proxy) (proxy.Proxy, error) {
+func NewShadowsocks(cipherName string, password string, server, port string) func(proxy.Proxy) (proxy.Proxy, error) {
 	return func(p proxy.Proxy) (proxy.Proxy, error) {
 		cipher, err := core.PickCipher(strings.ToUpper(cipherName), nil, password)
 		if err != nil {
 			return nil, err
 		}
-		return &Shadowsocks{
-			cipher: cipher,
-			server: server,
-			port:   port,
 
-			p: p,
-		}, nil
+		addr, err := net.ResolveUDPAddr("udp", net.JoinHostPort(server, port))
+		if err != nil {
+			return nil, fmt.Errorf("resolve udp addr failed: %v", err)
+		}
+
+		return &Shadowsocks{cipher: cipher, server: server, port: port, p: p, udpAddr: addr}, nil
 	}
 }
 
@@ -103,10 +57,6 @@ func (s *Shadowsocks) Conn(host string) (conn net.Conn, err error) {
 		_ = x.SetKeepAlive(true)
 	}
 
-	conn, err = s.pluginFunc(conn)
-	if err != nil {
-		return nil, fmt.Errorf("plugin exec failed: %v", err)
-	}
 	conn = s.cipher.StreamConn(conn)
 
 	target, err := ss5client.ParseAddr(host)
@@ -121,28 +71,24 @@ func (s *Shadowsocks) Conn(host string) (conn net.Conn, err error) {
 }
 
 //PacketConn .
-func (s *Shadowsocks) PacketConn(string) (net.PacketConn, error) {
-	ip, err := net.ResolveIPAddr("ip", s.server)
-	if err != nil {
-		return nil, fmt.Errorf("resolve ip failed: %v", err)
-	}
-	addr, err := net.ResolveUDPAddr("udp", net.JoinHostPort(ip.String(), s.port))
-	if err != nil {
-		return nil, fmt.Errorf("resolve udp addr failed: %v", err)
-	}
-
+func (s *Shadowsocks) PacketConn(tar string) (net.PacketConn, error) {
 	pc, err := s.p.PacketConn(s.server)
 	if err != nil {
 		return nil, fmt.Errorf("create packet conn failed")
 	}
 	pc = s.cipher.PacketConn(pc)
 
-	return &ssPacketConn{PacketConn: pc, add: addr}, nil
+	addr, err := ss5client.ParseAddr(tar)
+	if err != nil {
+		return nil, fmt.Errorf("parse target failed: %v", err)
+	}
+	return &ssPacketConn{PacketConn: pc, add: s.udpAddr, target: addr}, nil
 }
 
 type ssPacketConn struct {
 	net.PacketConn
-	add net.Addr
+	add    net.Addr
+	target []byte
 }
 
 func (v *ssPacketConn) ReadFrom(b []byte) (int, net.Addr, error) {
@@ -165,10 +111,6 @@ func (v *ssPacketConn) ReadFrom(b []byte) (int, net.Addr, error) {
 	return n - addrSize, addr, nil
 }
 
-func (v *ssPacketConn) WriteTo(b []byte, addr net.Addr) (int, error) {
-	target, err := ss5client.ParseAddr(addr.String())
-	if err != nil {
-		return 0, fmt.Errorf("parse target failed: %v", err)
-	}
-	return v.PacketConn.WriteTo(bytes.Join([][]byte{target, b}, []byte{}), v.add)
+func (v *ssPacketConn) WriteTo(b []byte, _ net.Addr) (int, error) {
+	return v.PacketConn.WriteTo(bytes.Join([][]byte{v.target, b}, []byte{}), v.add)
 }
