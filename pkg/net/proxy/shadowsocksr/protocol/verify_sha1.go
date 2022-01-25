773b473a29469d6e45498cc151b132e27a115a5f
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2022-01-25 15:12:27 +0800

    rewrite shadowsocksr

---
+++ pkg/net/proxy/shadowsocksr/protocol/verify_sha1.go
@@ -0,0 +1,116 @@
+package protocol
+
+import (
+	"bytes"
+	"encoding/binary"
+
+	ssr "github.com/Asutorufa/yuhaiin/pkg/net/proxy/shadowsocksr/utils"
+)
+
+func init() {
+	register("verify_sha1", NewVerifySHA1)
+	register("ota", NewVerifySHA1)
+}
+
+type verifySHA1 struct {
+	ssr.ServerInfo
+	hasSentHeader bool
+	buffer        bytes.Buffer
+	chunkId       uint32
+}
+
+const (
+	oneTimeAuthMask byte = 0x10
+)
+
+func NewVerifySHA1(info ssr.ServerInfo) IProtocol {
+	a := &verifySHA1{
+		ServerInfo: info,
+	}
+	a.Overhead = 0
+	return a
+}
+
+func (v *verifySHA1) otaConnectAuth(data []byte) []byte {
+	return append(data, ssr.HmacSHA1(append(v.IV, v.Key...), data)...)
+}
+
+func (v *verifySHA1) otaReqChunkAuth(chunkId uint32, data []byte) []byte {
+	nb := make([]byte, 2)
+	binary.BigEndian.PutUint16(nb, uint16(len(data)))
+	chunkIdBytes := make([]byte, 4)
+	binary.BigEndian.PutUint32(chunkIdBytes, chunkId)
+	header := append(nb, ssr.HmacSHA1(append(v.IV, chunkIdBytes...), data)...)
+	return append(header, data...)
+}
+
+func (v *verifySHA1) otaVerifyAuth(iv []byte, chunkId uint32, data []byte, expectedHmacSha1 []byte) bool {
+	chunkIdBytes := make([]byte, 4)
+	binary.BigEndian.PutUint32(chunkIdBytes, chunkId)
+	actualHmacSha1 := ssr.HmacSHA1(append(iv, chunkIdBytes...), data)
+	return bytes.Equal(expectedHmacSha1, actualHmacSha1)
+}
+
+func (v *verifySHA1) getAndIncreaseChunkId() (chunkId uint32) {
+	chunkId = v.chunkId
+	v.chunkId += 1
+	return
+}
+
+func (v *verifySHA1) SetServerInfo(s *ssr.ServerInfo) {
+	v.ServerInfo = *s
+}
+
+func (v *verifySHA1) GetServerInfo() (s *ssr.ServerInfo) {
+	return &v.ServerInfo
+}
+
+func (v *verifySHA1) SetData(data interface{}) {
+
+}
+
+func (v *verifySHA1) GetData() interface{} {
+	return nil
+}
+
+func (v *verifySHA1) PreEncrypt(data []byte) (encryptedData []byte, err error) {
+	v.buffer.Reset()
+	dataLength := len(data)
+	offset := 0
+	if !v.hasSentHeader {
+		data[0] |= oneTimeAuthMask
+		v.buffer.Write(v.otaConnectAuth(data[:v.HeadLen]))
+		v.hasSentHeader = true
+		dataLength -= v.HeadLen
+		offset += v.HeadLen
+	}
+	const blockSize = 4096
+	for dataLength > blockSize {
+		chunkId := v.getAndIncreaseChunkId()
+		v.buffer.Write(v.otaReqChunkAuth(chunkId, data[offset:offset+blockSize]))
+		dataLength -= blockSize
+		offset += blockSize
+	}
+	if dataLength > 0 {
+		chunkId := v.getAndIncreaseChunkId()
+		v.buffer.Write(v.otaReqChunkAuth(chunkId, data[offset:]))
+	}
+	return v.buffer.Bytes(), nil
+}
+
+func (v *verifySHA1) PostDecrypt(data []byte) ([]byte, int, error) {
+	return data, len(data), nil
+}
+
+func (v *verifySHA1) GetOverhead() int {
+	return 0
+}
+
+func (v *verifySHA1) AddOverhead(int) {}
+
+func (a *verifySHA1) PreEncryptPacket(b []byte) ([]byte, error) {
+	return b, nil
+}
+func (a *verifySHA1) PostDecryptPacket(b []byte) ([]byte, error) {
+	return b, nil
+}
