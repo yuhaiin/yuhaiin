bdb8e893e225bd1494a8fea3e46fb0cfbf4798c4
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2022-04-07 18:06:31 +0800

    :art: optimize subscr
    Signed-off-by: asutorufa <16442314+Asutorufa@users.noreply.github.com>

---
+++ pkg/subscr/manager.go
@@ -0,0 +1,137 @@
+package subscr
+
+import (
+	"sync"
+)
+
+type manager struct {
+	*Manager
+	lock sync.RWMutex
+}
+
+func (m *manager) GetNode(hash string) (*Point, bool) {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+	p, ok := m.Nodes[hash]
+	return p, ok
+}
+
+func (m *manager) GetNodeByName(group, name string) (*Point, bool) {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+	z := m.GroupNodesMap[group]
+	if z == nil {
+		return nil, false
+	}
+
+	hash := z.NodeHashMap[name]
+	if hash != "" {
+		return nil, false
+	}
+
+	return m.GetNode(hash)
+}
+
+func (m *manager) AddNode(p *Point) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	if m.Nodes == nil {
+		m.Nodes = make(map[string]*Point)
+	}
+
+	_, ok := m.GroupNodesMap[p.Group]
+	if !ok {
+		m.GroupNodesMap[p.Group] = &ManagerNodeArray{
+			Group:       p.Group,
+			Nodes:       make([]string, 0),
+			NodeHashMap: make(map[string]string),
+		}
+		m.Groups = append(m.Groups, p.Group)
+	}
+
+	m.GroupNodesMap[p.Group].NodeHashMap[p.Name] = p.Hash
+	m.GroupNodesMap[p.Group].Nodes = append(m.GroupNodesMap[p.Group].Nodes, p.Name)
+
+	m.Nodes[p.Hash] = p
+}
+
+func (n *manager) DeleteRemoteNodes(group string) {
+	n.lock.Lock()
+	defer n.lock.Unlock()
+
+	m := n.Manager
+
+	x, ok := m.GroupNodesMap[group]
+	if !ok {
+		return
+	}
+
+	ns := x.Nodes
+	msmap := x.NodeHashMap
+	left := make([]string, 0)
+	for i := range ns {
+		if m.Nodes[msmap[ns[i]]].GetOrigin() != Point_remote {
+			left = append(left, ns[i])
+			continue
+		}
+
+		delete(m.Nodes, msmap[ns[i]])
+		delete(m.GroupNodesMap[group].NodeHashMap, ns[i])
+	}
+
+	if len(left) == 0 {
+		delete(m.GroupNodesMap, group)
+		for i, x := range m.Groups {
+			if x != group {
+				continue
+			}
+
+			m.Groups = append(m.Groups[:i], m.Groups[i+1:]...)
+			break
+		}
+		return
+	}
+
+	m.GroupNodesMap[group].Nodes = left
+}
+
+func (m *manager) DeleteNode(hash string) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	p, ok := m.Nodes[hash]
+	if !ok {
+		return
+	}
+
+	delete(m.Nodes, hash)
+	delete(m.GroupNodesMap[p.Group].NodeHashMap, p.Name)
+
+	for i, x := range m.GroupNodesMap[p.Group].Nodes {
+		if x != p.Name {
+			continue
+		}
+
+		m.GroupNodesMap[p.Group].Nodes = append(
+			m.GroupNodesMap[p.Group].Nodes[:i],
+			m.GroupNodesMap[p.Group].Nodes[i+1:]...,
+		)
+		break
+	}
+
+	if len(m.GroupNodesMap[p.Group].Nodes) != 0 {
+		return
+	}
+
+	delete(m.GroupNodesMap, p.Group)
+
+	for i, x := range m.Groups {
+		if x != p.Group {
+			continue
+		}
+
+		m.Groups = append(m.Groups[:i], m.Groups[i+1:]...)
+		break
+	}
+}
