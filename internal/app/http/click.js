cce9e0ecddc5e10e8327b1e4963899cc76e2da5f
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2022-04-09 13:23:29 +0800

    add a simple html page
    Signed-off-by: asutorufa <16442314+Asutorufa@users.noreply.github.com>

---
+++ internal/app/http/click.js
@@ -0,0 +1,14 @@
+function latency(id, hash) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", "/latency?hash=" + hash, true);
+    xmlhttp.send();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            console.log('get data：' + JSON.stringify(xmlhttp.responseText));
+
+            document.getElementById(id).innerHTML = xmlhttp.responseText;
+        } else {
+            document.getElementById(id).innerHTML = '9999.99ms';
+        }
+    }
+}
