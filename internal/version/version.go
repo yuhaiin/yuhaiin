33a6e1f6925e9e40025a1768c8bff6058e6feea8
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2022-05-14 12:57:07 +0800

    add version
    Signed-off-by: asutorufa <16442314+Asutorufa@users.noreply.github.com>

---
+++ internal/version/version.go
@@ -0,0 +1,45 @@
+package version
+
+import (
+	"runtime"
+	"strings"
+)
+
+var (
+	// Version can be set at link time by executing
+	// the command: `git describe --abbrev=0 --tags HEAD`
+	Version string
+
+	// GitCommit can be set at link time by executing
+	// the command: `git rev-parse --short HEAD`
+	GitCommit string
+
+	BuildArch string
+	BuildTime string
+)
+
+func String() string {
+	str := strings.Builder{}
+
+	str.WriteString("version: ")
+	str.WriteString(Version)
+	str.WriteString("\n")
+
+	str.WriteString("commit: ")
+	str.WriteString(GitCommit)
+	str.WriteString("\n")
+
+	str.WriteString("build arch: ")
+	str.WriteString(BuildArch)
+	str.WriteString("\n")
+
+	str.WriteString("build time: ")
+	str.WriteString(BuildTime)
+	str.WriteString("\n")
+
+	str.WriteString("go version: ")
+	str.WriteString(runtime.Version())
+	str.WriteString("\n")
+
+	return str.String()
+}
