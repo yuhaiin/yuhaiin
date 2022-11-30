142968f48626be44ec896e04cc224e73304013d9
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2022-11-30 15:57:39 +0800

    make connections auto refresh
    Signed-off-by: asutorufa <16442314+Asutorufa@users.noreply.github.com>

--- internal/http/templates/tps.go
+++ internal/http/templates/tps.go
@@ -10,13 +10,12 @@ var Pages embed.FS
 const (
 	FRAME = "http.html"
 
-	GROUP       = "group.html"
-	GROUP_LIST  = "grouplist.html"
-	ROOT        = "root.html"
-	STATISTIC   = "statistic.html"
-	SUB         = "sub.html"
-	NEW_NODE    = "newnode.html"
-	CONNECTIONS = "connections.html"
-	EMPTY_BODY  = "body.html"
-	CONFIG      = "config.html"
+	GROUP      = "group.html"
+	GROUP_LIST = "grouplist.html"
+	ROOT       = "root.html"
+	STATISTIC  = "statistic.html"
+	SUB        = "sub.html"
+	NEW_NODE   = "newnode.html"
+	EMPTY_BODY = "body.html"
+	CONFIG     = "config.html"
 )
--- internal/shunt/rule.go
+++ internal/shunt/rule.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 
 	"github.com/Asutorufa/yuhaiin/pkg/log"
+	"github.com/Asutorufa/yuhaiin/pkg/utils/yerror"
 )
 
 //go:embed statics/bypass.gz
@@ -21,34 +22,14 @@ func rangeRule(path string, ranger func(string, string)) {
 	reader, err = os.Open(path)
 	if err != nil {
 		log.Errorf("open bypass file %s failed: %v, fallback to use internal bypass data", path, err)
-
 		reader, _ = gzip.NewReader(bytes.NewReader(BYPASS_DATA))
 	}
-
 	defer reader.Close()
 
 	br := bufio.NewScanner(reader)
-	for {
-		if !br.Scan() {
-			break
-		}
-
-		a := br.Bytes()
-
-		i := bytes.IndexByte(a, '#')
-		if i != -1 {
-			a = a[:i]
-		}
-
-		i = bytes.IndexByte(a, ' ')
-		if i == -1 {
-			continue
-		}
-
-		c, b := a[:i], a[i+1:]
-
-		if len(c) != 0 && len(b) != 0 {
-			ranger(strings.ToLower(string(c)), strings.ToLower(string(b)))
+	for br.Scan() {
+		if fields := bytes.Fields(yerror.Ignore2(bytes.Cut(br.Bytes(), []byte{'#'}))); len(fields) >= 2 {
+			ranger(strings.ToLower(string(fields[0])), strings.ToLower(string(fields[1])))
 		}
 	}
 }
