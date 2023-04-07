e666caf35c4ea2fd7123ddaeb131d9e16ab964ad
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2023-04-08 02:00:56 +0800

    update

--- yuhaiin/update.sh
+++ yuhaiin/update.sh
@@ -29,7 +29,7 @@ cat pglyoyo.txt | sed $'s/\r$//' | sed '/ *[Adblock] */d' | sed 's/$/ BLOCK/g' >
 cat ja_ad | sed $'s/\r$//' | grep '#' | sed '1,9d' | sed 's/# /*\./g'| sed '/^ *$/d' | sed 's/$/ BLOCK/g' >> yuhaiin_ad.conf
 # cat ja_ad | sed $'s/\r$//' | sed 's/127.0.0.1 //g' | sed '/#/'d  | sed '/^\s*$/d'  |sed 's/$/ BLOCK/g' >> yuhaiin_ad.conf
 cat ad_wars_hosts | sed $'s/\r$//' | sed 's/127.0.0.1 //g' | sed '/^#.*$/'d | sed '1,2d' | sed '/^ *$/d' |sed 's/$/ BLOCK/g' >> yuhaiin_ad.conf
-cat cn_ad.conf  | sed $'s/\r$//' | sed '/#/'d  | sed '/^\s*$/d'  |sed 's/$/ BLOCK/g' >> yuhaiin_ad.conf
+# cat cn_ad.conf  | sed $'s/\r$//' | sed '/#/'d  | sed '/^\s*$/d'  |sed 's/$/ BLOCK/g' >> yuhaiin_ad.conf
 cat ja_ad_allow | sed $'s/\r$//' | sed 's/127.0.0.1 //g' | sed '/^#.*$/'d  | sed '/^\s*$/d'  |sed 's/$/ PROXY/g' >> yuhaiin_ad.conf
 cat yuhaiin_ad.conf | sort -u | uniq > yuhaiin_ad_tmp.conf
 rm yuhaiin_ad.conf
