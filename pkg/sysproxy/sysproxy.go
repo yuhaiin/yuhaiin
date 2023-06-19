9ce6cb339976f8448c683bcd0fbe8325bafc1322
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2023-06-19 10:24:39 +0800

    fix sysproxy unspecified ip address bug
    Signed-off-by: Asutorufa <16442314+Asutorufa@users.noreply.github.com>

--- pkg/sysproxy/sysproxy.go
+++ pkg/sysproxy/sysproxy.go
@@ -1,6 +1,10 @@
 package sysproxy
 
 import (
+	"net"
+	"net/netip"
+	"strconv"
+
 	cb "github.com/Asutorufa/yuhaiin/pkg/protos/config"
 	"github.com/Asutorufa/yuhaiin/pkg/protos/config/listener"
 	"google.golang.org/protobuf/proto"
@@ -24,8 +28,24 @@ func Update(s *cb.Setting) {
 			socks5 = v.GetSocks5().GetHost()
 		}
 	}
-	SetSysProxy(http, socks5)
+
+	SetSysProxy(replaceUnspecified(http), replaceUnspecified(socks5))
 	server = s.Server
+}
+
+func replaceUnspecified(s string) string {
+	if ip, err := netip.ParseAddrPort(s); err == nil {
+		if ip.Addr().IsUnspecified() {
+			if ip.Addr().Is6() {
+				return net.JoinHostPort(net.IPv6loopback.String(), strconv.Itoa(int(ip.Port())))
+			} else {
+				return net.JoinHostPort("127.0.0.1", strconv.Itoa(int(ip.Port())))
+
+			}
+		}
+	}
+
+	return s
 }
 
 func Unset() {
