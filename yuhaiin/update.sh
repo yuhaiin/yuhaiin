baa3230a1993cb6a17b1225f732a4c1e392b605c
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2023-12-22 19:28:49 +0800

    update

--- yuhaiin/update.sh
+++ yuhaiin/update.sh
@@ -3,6 +3,8 @@ wget https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/goo
 wget https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf -O accelerated-domains.china.conf
 wget https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts -O ad_wars_hosts
 wget "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=adblock&showintro=0&mimetype=plaintext" -O pglyoyo.txt
+wget "https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-domains.txt" -O anti-ad-domains.txt
+
 # wget https://logroid.github.io/adaway-hosts/hosts.txt -O ja_ad
 # wget https://logroid.github.io/adaway-hosts/hosts_allow.txt -O ja_ad_allow
 touch ja_ad ja_ad_allow
@@ -25,13 +27,14 @@ cat stream.conf >> yuhaiin.conf
 cat bt.conf >> yuhaiin.conf
 
 # AD
-cat pglyoyo.txt | sed $'s/\r$//' | sed '/ *[Adblock] */d' | sed 's/$/ BLOCK/g' > yuhaiin_ad.conf
+cat anti-ad-domains.txt | sed $'s/\r$//' | sed '/^#/d' | sed 's/#.*$//g' | sed 's/# /*\./g' | sed '/^ *$/d' | sed 's/^/*./g'  | sed 's/$/ BLOCK/g' > yuhaiin_ad.conf
+cat pglyoyo.txt | sed $'s/\r$//' | sed '/ *[Adblock] */d' | sed 's/$/ BLOCK/g' >> yuhaiin_ad.conf
 cat ja_ad | sed $'s/\r$//' | grep '#' | sed '1,9d' | sed 's/# /*\./g'| sed '/^ *$/d' | sed 's/$/ BLOCK/g' >> yuhaiin_ad.conf
 # cat ja_ad | sed $'s/\r$//' | sed 's/127.0.0.1 //g' | sed '/#/'d  | sed '/^\s*$/d'  |sed 's/$/ BLOCK/g' >> yuhaiin_ad.conf
 cat ad_wars_hosts | sed $'s/\r$//' | sed 's/127.0.0.1 //g' | sed '/^#.*$/'d | sed '1,2d' | sed '/^ *$/d' |sed 's/$/ BLOCK/g' >> yuhaiin_ad.conf
 # cat cn_ad.conf  | sed $'s/\r$//' | sed '/#/'d  | sed '/^\s*$/d'  |sed 's/$/ BLOCK/g' >> yuhaiin_ad.conf
 cat ja_ad_allow | sed $'s/\r$//' | sed 's/127.0.0.1 //g' | sed '/^#.*$/'d  | sed '/^\s*$/d'  |sed 's/$/ PROXY/g' >> yuhaiin_ad.conf
-cat yuhaiin_ad.conf | sort -u | uniq > yuhaiin_ad_tmp.conf
+cat yuhaiin_ad.conf | sed '/^#/d' | sed 's/#.*$//g' | sed '/^ *$/d' | sort -u | uniq > yuhaiin_ad_tmp.conf
 rm yuhaiin_ad.conf
 mv yuhaiin_ad_tmp.conf yuhaiin_ad.conf
 cat yuhaiin.conf >> yuhaiin_ad.conf
@@ -46,4 +49,4 @@ cat yuhaiin_ad.conf > yuhaiin_my.conf
 cat private.conf >> yuhaiin_my.conf
 
 # DELETE
-rm apple.china.conf google.china.conf accelerated-domains.china.conf ad_wars_hosts pglyoyo.txt ja_ad ja_ad_allow
+rm apple.china.conf google.china.conf accelerated-domains.china.conf ad_wars_hosts pglyoyo.txt ja_ad ja_ad_allow anti-ad-domains.txt
