6a72da7768825ea828100fda26beefd4d2d5578d
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2025-09-27 12:57:23 +0800

    optimize logger controller
    Signed-off-by: Asutorufa <16442314+Asutorufa@users.noreply.github.com>

--- pkg/net/netapi/conn.go
+++ pkg/net/netapi/conn.go
@@ -2,50 +2,8 @@ package netapi
 
 import (
 	"errors"
-	"fmt"
-	"net"
-	"runtime"
 	"syscall"
-	"time"
 )
-
-type LogConn struct {
-	net.Conn
-}
-
-func (l *LogConn) Write(b []byte) (int, error) {
-	return l.Conn.Write(b)
-}
-
-func (l *LogConn) Read(b []byte) (int, error) {
-	n, err := l.Conn.Read(b)
-	if err != nil {
-		fmt.Println("tls read failed", "err", err)
-	}
-
-	return n, err
-}
-
-func (l *LogConn) SetDeadline(t time.Time) error {
-	_, file, line, _ := runtime.Caller(3)
-	fmt.Println("set deadline", "time", t, "line", line, "file", file, "time", t)
-
-	return l.Conn.SetDeadline(t)
-}
-
-func (l *LogConn) SetReadDeadline(t time.Time) error {
-	_, file, line, _ := runtime.Caller(3)
-	fmt.Println("set read deadline", "time", t, "line", line, "file", file, "time", t)
-
-	return l.Conn.SetReadDeadline(t)
-}
-
-func (l *LogConn) SetWriteDeadline(t time.Time) error {
-	_, file, line, _ := runtime.Caller(4)
-	fmt.Println("set write deadline", "time", t, "line", line, "file", file, "time", t)
-
-	return l.Conn.SetWriteDeadline(t)
-}
 
 func IsConnectionTimedout(err error) bool {
 	var se syscall.Errno
