dd6baf668ee54dea74ca4e25a11e5856545c1ae4
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2025-10-25 01:36:15 +0800

    migrate s3 to simple s3 client
    Signed-off-by: Asutorufa <16442314+Asutorufa@users.noreply.github.com>

--- pkg/node/subscribe.go
+++ pkg/node/subscribe.go
@@ -10,6 +10,7 @@ import (
 	"log/slog"
 	"net"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/Asutorufa/yuhaiin/internal/version"
@@ -19,6 +20,9 @@ import (
 	"github.com/Asutorufa/yuhaiin/pkg/node/parser"
 	"github.com/Asutorufa/yuhaiin/pkg/protos/api"
 	"github.com/Asutorufa/yuhaiin/pkg/protos/node"
+	"github.com/Asutorufa/yuhaiin/pkg/utils/system"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/wrapperspb"
@@ -74,7 +78,14 @@ func (l *Subscribe) update(names ...string) {
 			continue
 		}
 
-		if err := l.fetch(link); err != nil {
+		scheme, _, _ := system.GetScheme(link.GetUrl())
+		var err error
+		if scheme == "yuhaiin" {
+			err = l.savePublish(context.TODO(), link.GetUrl())
+		} else {
+			err = l.fetch(link)
+		}
+		if err != nil {
 			log.Error("get one link failed", "err", err)
 		}
 	}
@@ -166,4 +177,73 @@ func (n *Subscribe) Publish(ctx context.Context, in *api.PublishRequest) (*api.P
 	return api.PublishResponse_builder{
 		Points: n.n.Publish(in.GetName(), in.GetPath(), in.GetPassword()),
 	}.Build(), nil
+}
+
+func (n *Subscribe) savePublish(ctx context.Context, url string) error {
+	u := strings.TrimPrefix(url, "yuhaiin://")
+
+	data, err := base64.RawURLEncoding.DecodeString(u)
+	if err != nil {
+		return err
+	}
+
+	yu := &node.YuhaiinUrl{}
+	if err = proto.Unmarshal(data, yu); err != nil {
+		return err
+	}
+
+	if yu.GetName() == "" {
+		yu.SetName("default")
+	}
+
+	switch yu.WhichUrl() {
+	case node.YuhaiinUrl_Points_case:
+		n.n.SaveNode(yu.GetPoints().GetPoints()...)
+	case node.YuhaiinUrl_Remote_case:
+		u := yu.GetRemote().GetUrl()
+		opts := []grpc.DialOption{
+			grpc.WithContextDialer(func(ctx context.Context, s string) (net.Conn, error) {
+				ad, err := netapi.ParseAddress("tcp", s)
+				if err != nil {
+					return nil, fmt.Errorf("parse address failed: %w", err)
+				}
+
+				ctx = netapi.WithContext(ctx)
+
+				return configuration.ProxyChain.Conn(ctx, ad)
+			}),
+		}
+		if yu.GetRemote().GetInsecure() {
+			opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		}
+
+		c, err := grpc.NewClient(u, opts...)
+		if err != nil {
+			return fmt.Errorf("new client failed: %w", err)
+		}
+		defer c.Close()
+
+		sbc := api.NewSubscribeClient(c)
+
+		resp, err := sbc.Publish(ctx, api.PublishRequest_builder{
+			Name:     proto.String(yu.GetRemote().GetPublish().GetName()),
+			Path:     proto.String(yu.GetRemote().GetPublish().GetPath()),
+			Password: proto.String(yu.GetRemote().GetPublish().GetPassword()),
+		}.Build())
+		if err != nil {
+			return fmt.Errorf("publish failed: %w", err)
+		}
+
+		for _, p := range resp.GetPoints() {
+			p.SetOrigin(node.Origin_remote)
+			p.SetGroup(yu.GetName())
+		}
+
+		n.n.SaveNode(resp.GetPoints()...)
+
+	default:
+		return fmt.Errorf("unknown url type")
+	}
+
+	return n.n.Save()
 }
